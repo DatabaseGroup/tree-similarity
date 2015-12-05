5f5b2abd23a107a8773ed3e8715e51c9d7b41141
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-12-05 12:03:59 +0100

    Comply to c++ style guide

--- src/tree_edit_distance.h
+++ src/tree_edit_distance.h
@@ -1,19 +1,17 @@
-#ifndef TreeEditDistance_H
-#define TreeEditDistance_H
+#ifndef TREE_EDIT_DISTANCE_H
+#define TREE_EDIT_DISTANCE_H
 
-#include "LblTree.h"
+#include "lbl_tree.h"
 
-class TreeEditDistance{
-    public:
-        int w_insert, w_delete, w_rename;
+class tree_edit_distance {
+public:
+  int w_insert, w_delete, w_rename;
 
-        TreeEditDistance(int ins, int del, int ren){
-            w_insert = ins;
-            w_delete = del;
-            w_rename = ren;
-         }
-        ~TreeEditDistance(){};
-        virtual int calculateDistance(LblTree*, LblTree*, bool)=0; //=0 means that calculateDistnace is not implemented here
+  tree_edit_distance (int ins, int del, int ren)
+    : w_insert(ins), w_delete(del), w_rename(ren) { }
+  
+  ~tree_edit_distance () { };
+  virtual int calculate_distance (lbl_tree*, lbl_tree*, bool) = 0;
 };
 
-#endif // TreeEditDistance_H
+#endif // TREE_EDIT_DISTANCE_H
