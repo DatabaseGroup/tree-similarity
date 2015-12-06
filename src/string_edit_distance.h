b45198f76840f431e348d4f99bf6a6a0c648ca24
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-12-06 22:02:16 +0100

    Update to fixed strategy pattern

--- src/string_edit_distance.h
+++ src/string_edit_distance.h
@@ -3,16 +3,16 @@
 
 #include <string>
 #include <iostream>
-#include "tree_edit_distance.h"
+#include "tree_edit_distance_strategy.h"
 
-class string_edit_distance : public tree_edit_distance {
+class string_edit_distance : public tree_edit_distance_strategy {
 private:
   std::vector<int> s1;
   std::vector<int> s2;
 
 public:
   string_edit_distance (int ins, int del, int ren)
-    : tree_edit_distance(ins, del, ren) { }
+    : tree_edit_distance_strategy(ins, del, ren) { }
   virtual ~string_edit_distance () { };
 
   int calculate_distance(lbl_tree*, lbl_tree*, bool);
