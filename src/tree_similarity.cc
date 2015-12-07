4738932ca2f055c214bd581b43b95a02a43a7783
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-12-07 23:59:40 +0100

    Comply to C++ style guide

--- src/tree_similarity.cc
+++ src/tree_similarity.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "node.h"
 #include "upper_bound.h"
 #include "string_edit_distance.h"
@@ -7,27 +8,27 @@
 int main(int argc, char** argv) {
 	// create test-trees for simplyUpperBound
 	node* t1 = new node(1, 5);
-  node* middleChild = new node(3);
+  node* middle_child = new node(3);
 	
   t1->add_child(new node(1));
-	middleChild->add_child(new node(2));
-	t1->add_child(middleChild);
+	middle_child->add_child(new node(2));
+	t1->add_child(middle_child);
 	t1->add_child(new node(4));
 
   node* t2 = new node(13, 5);
-	node* leftChild = new node(4);
+	node* left_child = new node(4);
   
-  leftChild->add_child(new node(1));
-  leftChild->add_child(new node(2));
-  leftChild->add_child(new node(3));
-  t2->add_child(leftChild);
+  left_child->add_child(new node(1));
+  left_child->add_child(new node(2));
+  left_child->add_child(new node(3));
+  t2->add_child(left_child);
 
   upper_bound* ub = new upper_bound();
   int sup = ub->calculate_simply_upper_bound(t1, t2);
   std::cout << "UpperBound t1->t2: " << sup << std::endl;
 
   int sed_result;
-  node* string1 = new node(3,13);
+  node* string1 = new node(3, 13);
   node* char2 = new node(15);
   node* char3 = new node(14);
   char3->add_child(new node(4));
@@ -42,7 +43,8 @@ int main(int argc, char** argv) {
   string2->add_child(char22);
 
   //should be 2
-  sed_result = sed::compute_string_edit_distance<node, costs<node>>(string1, string2);
+  sed_result =
+    sed::compute_string_edit_distance<node, costs<node>>(string1, string2);
   std::cout << "String Edit Distance: " << sed_result << std::endl;
 
   // Zhang and Shasha cost = 1 (insert), 1 (delete), 1 (rename)
