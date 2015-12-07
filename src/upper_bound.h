8870dd4a573431613eea053852c8243fbf7d271e
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-12-07 23:52:54 +0100

    Update to use node class only

--- src/upper_bound.h
+++ src/upper_bound.h
@@ -1,13 +1,13 @@
 #ifndef UPPER_BOUND_H
 #define UPPER_BOUND_H
 
-#include "lbl_tree.h"
+#include "node.h"
 
 class upper_bound {
 public:
 	upper_bound () { };
 	virtual ~upper_bound () { };
-	int calculate_simply_upper_bound (lbl_tree* t1, lbl_tree* t2);
+	int calculate_simply_upper_bound (node* t1, node* t2);
 };
 
 #endif // UPPER_BOUND_H
