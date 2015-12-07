56379fc9954ed561dbff35270aa2010b5e8cdf56
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-12-07 23:53:11 +0100

    Update to use node class only

--- src/upper_bound.cc
+++ src/upper_bound.cc
@@ -1,5 +1,5 @@
 #include "upper_bound.h"
 
-int upper_bound::calculate_simply_upper_bound (lbl_tree* t1, lbl_tree* t2) {
+int upper_bound::calculate_simply_upper_bound (node* t1, node* t2) {
 	return (t1->get_subtree_size() + t2->get_subtree_size());
 }
