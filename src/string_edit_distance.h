a271fa62faa19564e1bfae24a81cfdb313c055de
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-12-07 23:52:11 +0100

    Replace with template function

--- src/string_edit_distance.h
+++ src/string_edit_distance.h
@@ -1,21 +1,77 @@
 #ifndef STRING_EDIT_DISTANCE_H
 #define STRING_EDIT_DISTANCE_H
 
-#include <string>
+#include <vector>
 #include <iostream>
-#include "tree_edit_distance_strategy.h"
 
-class string_edit_distance : public tree_edit_distance_strategy {
-private:
-  std::vector<int> s1;
-  std::vector<int> s2;
+#include "node.h"
 
-public:
-  string_edit_distance (int ins, int del, int ren)
-    : tree_edit_distance_strategy(ins, del, ren) { }
-  virtual ~string_edit_distance () { };
+namespace sed {
 
-  int calculate_distance(lbl_tree*, lbl_tree*, bool);
+/**
+ * Generic function to compute the distance between two trees under a specified
+ * cost model. Each tree is represented by its respective root node. A root node
+ * type is specified as first template parameter, the cost model type as second
+ * template parameter.
+ */
+template<class _node = node, class _costs = costs<_node>>
+int compute_string_edit_distance (_node* t1, _node* t2, _costs c = _costs()) {
+  std::vector<int> s1, s2;
+  int c0 = 0, temp = 0;
+
+  //check if input is a string
+  node* temp_node = t1;
+  while (temp_node->get_children_number() == 1) {
+    s1.push_back(temp_node->get_label_id());
+    temp_node = temp_node->get_child(0);
+  }
+
+  if (temp_node->get_children_number() != 0) {
+    //false input
+    std::cout << "Error: first input is not a string!" << std::endl;
+    return -1;
+  } else {
+    s1.push_back(temp_node->get_label_id());
+  }
+
+  temp_node = t2;
+  while (temp_node->get_children_number() == 1) {
+    s2.push_back(temp_node->get_label_id());
+    temp_node = temp_node->get_child(0);
+  }
+
+  if (temp_node->get_children_number() != 0) {
+    //false input
+    std::cout << "Error: second input is not a string!" << std::endl;
+    return -1;
+  } else {
+    s2.push_back(temp_node->get_label_id());
+  }
+
+  //calculate string edit distance
+  int result[t1->get_subtree_size()][t2->get_subtree_size()];
+  for (int i = 0; i < t1->get_subtree_size(); ++i) {
+    result[i][0] = i;
+  }
+
+  for (int i = 0; i < t2->get_subtree_size(); ++i) {
+    result[0][i] = i;
+  }
+
+  for (int j = 1; j < t2->get_subtree_size(); ++j) {
+    for (int i = 1; i < t1->get_subtree_size(); ++i) {
+      c0 = (s1.at(i) == s2.at(j)) ? 0 : 1;
+
+      //calculate minimum
+      temp = (result[i - 1][j - 1] + c0 < result[i - 1][j] + 1) ?
+        result[i - 1][j - 1] + c0 : result[i - 1][j] + 1;
+      result[i][j] = (temp < result[i][j - 1] + 1) ? temp : result[i][j - 1] + 1;
+    }
+  }
+
+  return result[t1->get_subtree_size() - 1][t2->get_subtree_size() - 1];
+}
+
 };
 
 #endif // STRING_EDIT_DISTANCE_H
