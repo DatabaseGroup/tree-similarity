c6623be6e595df319cde2b684ee01a2d7e091030
Daniel Kocher <dkocher@cosy.sbg.ac.at>
2016-07-13 11:18:24 +0200

    Separate declaration and definition

--- src/node.h
+++ src/node.h
@@ -25,98 +25,48 @@ private:
 
 public:
   // Constructors
-  Node (int id, int label_id, int children_number)
-    : id_(id), label_id_(label_id), children_number_(children_number)
-  { }
-
-  explicit Node (int label_id)
-    : Node(0, label_id)
-  { }
-
-  Node (int id, int label_id)
-    : Node(id, label_id, 0)
-  { }
+  Node (int id, int label_id, int children_number);
+  explicit Node (int label_id);
+  Node (int id, int label_id);
 
   //Node (Node&);
 
   // Destructor
-  ~Node () {
-    // delete all children nodes
-    for ( std::vector<Node*>::iterator node_it = children_.begin();
-          node_it != children_.end(); ++node_it)
-    {
-      delete *node_it;
-    }
+  ~Node ();
 
-    // clear vector to avoid dangling pointers
-    children_.clear();
-  }
-
-  void copy_node(Node *node) {
-    *node = *this;
-  }
+  void copy_node(Node *node);
 
   // Get id
-  int get_id () const {
-    return id_;
-  }
+  int get_id () const;
 
   // Get label_id
-  int get_label_id () const {
-    return label_id_;
-  }
+  int get_label_id () const;
 
   // Get children
-  std::vector<Node*> get_children () const {
-    return children_;
-  }
+  std::vector<Node*> get_children () const;
 
   // Get children_number
-  int get_children_number () const {
-    //return children_number;
-    return children_.size();
-  }
+  int get_children_number () const;
 
   // Add a child at last position.
   //
   // Params:  child A pointer to the node to be added
   //
   // Return:  None
-  void add_child (Node* child) {
-    children_.push_back(child);
-    //++children_number;
-  }
+  void add_child (Node* child);
 
-  void add_child_at(Node* child, int position) {
-    std::vector<Node*>::iterator it = children_.begin();
-    children_.insert(it + position, child);
-  }
+  void add_child_at(Node* child, int position);
 
   // Get subtree size rooted at this node.
   //
   // Return:  The size of the subtree rooted at this node (including this node)
-  int get_subtree_size () const {
-    int descendants_sum = 1;
-    // Sum up sizes of subtrees rooted at child nodes (number of descendants)
-    for ( std::vector<Node*>::const_iterator node_it = children_.cbegin();
-          node_it != children_.cend(); ++node_it)
-    {
-      descendants_sum = descendants_sum + (*node_it)->get_subtree_size();
-    }
-
-    // Add this node to subtree size.
-    return descendants_sum;
-  }
+  int get_subtree_size () const;
 
   // Setter id
-  void set_id (int id) {
-    id_ = id;
-  }
+  void set_id (int id);
 
   // Setter label_id
-  void set_label_id(int id) {
-    label_id_ = id;
-  }
+  void set_label_id(int id);
 
   // Get a pointer to a child at a given position.
   //
@@ -124,100 +74,206 @@ public:
   //
   // Return:  A pointer to the child at position i if it exists or nullptr
   //          otherwise
-  Node* get_child (int position) const {
-    return children_[position];
-  }
+  Node* get_child (int position) const;
 
-  void remove_child (int position) {
-    children_.erase(children_.begin() + position);
-  }
+  void remove_child (int position);
 
-  void remove_child (Node* n) {
-    std::vector<Node*>::iterator it = std::find(children_.begin(), children_.end(), n);
-    if (it != children_.end()){
-      children_.erase(it);
-    }
-  }
+  void remove_child (Node* n);
 
-  void remove_all_children() {
-    children_.clear();
-  }
+  void remove_all_children();
 
-  int get_child_position (Node* n) {
-    std::vector<Node*>::iterator it = std::find(children_.begin(), children_.end(), n);
-    if(it!=children_.end()){
-      return (it - children_.begin());
-    } else {
-      return -1;
-    }
-  }
+  int get_child_position (Node* n);
 
-  void swap_children (int pos, int currpos) {
-    iter_swap(children_.begin() + pos, children_.begin() + currpos);
-  }
+  void swap_children (int pos, int currpos);
 
   // deleting in terms of removing and this node (=parent) inherits its children
-  void delete_child (int pos) {
-    Node* n = get_child(pos);
-    for(int i = 0; i < n->get_children_number(); i++){
-      this->add_child_at(n->get_child(i), (pos + i));
-    }
-    // might this be useless, since the child is deleted anyway?
-    n->remove_all_children();
-    remove_child(n);
-  }
+  void delete_child (int pos);
 
-  void delete_child (Node* n) {
-    int pos = get_child_position(n);
-    delete_child(pos);
-  }
+  void delete_child (Node* n);
 
-  void add_edge (char colour) {
-    edges_.push_back(colour);
-  }
+  void add_edge (char colour);
 
-  void add_edge_at(char colour, int position) {
-    std::vector<char>::iterator it = edges_.begin();
-    edges_.insert(it + position, colour);
-  }
+  void add_edge_at(char colour, int position);
 
-  void remove_edge (int position) {
-    edges_.erase(edges_.begin() + position);
-  }
+  void remove_edge (int position);
 
-  void set_edge_colour (int position, char colour) {
-    edges_[position] = colour;
-  }
+  void set_edge_colour (int position, char colour);
 
-  char get_edge_colour (int position) {
-    return edges_[position];
-  }
+  char get_edge_colour (int position);
 
-  int get_edge_colours_count (char first, char second) {
-    int counter = 0;    
-    for(std::vector<char>::iterator it = edges_.begin(); it != edges_.end(); ++it) {
-      char tmp = *it;
-      if(tmp==first || tmp==second) { counter++; }
-    }
-    return counter;
-  }
+  int get_edge_colours_count (char first, char second);
 
-  void set_colour (char colour) {
-    colour_ = colour;
-  }
+  void set_colour (char colour);
 
-  char get_colour () {
-    return colour_;
-  }
+  char get_colour ();
 
-  void set_level (int l) {
-    level = l;
-  }
+  void set_level (int l);
 
-  int get_level () {
-    return level;
-  }
+  int get_level ();
 };
+
+Node::Node (int id, int label_id, int children_number)
+  : id_(id), label_id_(label_id), children_number_(children_number) {}
+
+Node::Node (int label_id) : Node(0, label_id) {}
+Node::Node (int id, int label_id) : Node(id, label_id, 0) {}
+
+Node::~Node () {
+  // delete all children nodes
+  for ( std::vector<Node*>::iterator node_it = children_.begin();
+        node_it != children_.end(); ++node_it)
+  {
+    delete *node_it;
+  }
+
+  // clear vector to avoid dangling pointers
+  children_.clear();
+}
+
+void Node::copy_node(Node *node) {
+  *node = *this;
+}
+
+int Node::get_id () const {
+  return id_;
+}
+
+int Node::get_label_id () const {
+  return label_id_;
+}
+
+std::vector<Node*> Node::get_children () const {
+  return children_;
+}
+
+int Node::get_children_number () const {
+  //return children_number;
+  return children_.size();
+}
+
+void Node::add_child (Node* child) {
+  children_.push_back(child);
+  //++children_number;
+}
+
+void Node::add_child_at(Node* child, int position) {
+  std::vector<Node*>::iterator it = children_.begin();
+  children_.insert(it + position, child);
+}
+
+int Node::get_subtree_size () const {
+  int descendants_sum = 1;
+  // Sum up sizes of subtrees rooted at child nodes (number of descendants)
+  for ( std::vector<Node*>::const_iterator node_it = children_.cbegin();
+        node_it != children_.cend(); ++node_it)
+  {
+    descendants_sum = descendants_sum + (*node_it)->get_subtree_size();
+  }
+
+  // Add this node to subtree size.
+  return descendants_sum;
+}
+
+void Node::set_id (int id) {
+  id_ = id;
+}
+
+void Node::set_label_id(int id) {
+  label_id_ = id;
+}
+
+Node* Node::get_child (int position) const {
+  return children_[position];
+}
+
+void Node::remove_child (int position) {
+  children_.erase(children_.begin() + position);
+}
+
+void Node::remove_child (Node* n) {
+  std::vector<Node*>::iterator it = std::find(children_.begin(), children_.end(), n);
+  if (it != children_.end()){
+    children_.erase(it);
+  }
+}
+
+void Node::remove_all_children() {
+  children_.clear();
+}
+
+int Node::get_child_position (Node* n) {
+  std::vector<Node*>::iterator it = std::find(children_.begin(), children_.end(), n);
+  if(it!=children_.end()){
+    return (it - children_.begin());
+  } else {
+    return -1;
+  }
+}
+
+void Node::swap_children (int pos, int currpos) {
+  iter_swap(children_.begin() + pos, children_.begin() + currpos);
+}
+
+void Node::delete_child (int pos) {
+  Node* n = get_child(pos);
+  for(int i = 0; i < n->get_children_number(); i++){
+    this->add_child_at(n->get_child(i), (pos + i));
+  }
+  // might this be useless, since the child is deleted anyway?
+  n->remove_all_children();
+  remove_child(n);
+}
+
+void Node::delete_child (Node* n) {
+  int pos = get_child_position(n);
+  delete_child(pos);
+}
+
+void Node::add_edge (char colour) {
+  edges_.push_back(colour);
+}
+
+void Node::add_edge_at(char colour, int position) {
+  std::vector<char>::iterator it = edges_.begin();
+  edges_.insert(it + position, colour);
+}
+
+void Node::remove_edge (int position) {
+  edges_.erase(edges_.begin() + position);
+}
+
+void Node::set_edge_colour (int position, char colour) {
+  edges_[position] = colour;
+}
+
+char Node::get_edge_colour (int position) {
+  return edges_[position];
+}
+
+int Node::get_edge_colours_count (char first, char second) {
+  int counter = 0;    
+  for(std::vector<char>::iterator it = edges_.begin(); it != edges_.end(); ++it) {
+    char tmp = *it;
+    if(tmp==first || tmp==second) { counter++; }
+  }
+  return counter;
+}
+
+void Node::set_colour (char colour) {
+  colour_ = colour;
+}
+
+char Node::get_colour () {
+  return colour_;
+}
+
+void Node::set_level (int l) {
+  level = l;
+}
+
+int Node::get_level () {
+  return level;
+}
 
 // Represents the cost functions to be used for the distance computation.
 // Costs are generic for different node classes. A cost model has to provide
@@ -226,7 +282,7 @@ public:
 //  - del(n) for deletion costs of a node n
 //  - ins(n) for insertion costs of a node n
 // All three cost functions must return an integer.
-template<class _node>
+template<typename _Node>
 struct Costs {
   // Basic rename cost function
   //
@@ -234,35 +290,63 @@ struct Costs {
   //          node2  The node having the desired name
   //
   // Return:  The cost of renaming node1 to node2
-  int ren (_node* node1, _node* node2) {
-    if (node1->get_label_id() == node2->get_label_id()) {
-      return 0;
-    }
-
-    return 1;
-  }
+  int ren (_Node* node1, _Node* node2);
 
   // Basic delete cost function
   //
   // Params:  node The node to be deleted
   //
   // Return:  The cost of deleting node
-  int del (_node node) { return 1; }
+  int del (_Node node);
 
   // Basic insert cost function
   //
   // Params:  node The node to be inserted
   //
   // Return:  The cost of inserting node
-  int ins (_node node) { return 1; }
+  int ins (_Node node);
 
   // Dummy methods
-  int ren() { return 1; }
-  int del() { return 1; }
-  int ins() { return 1; }
+  int ren ();
+  int del ();
+  int ins ();
 };
 
-template<class _node>
+template<typename _Node>
+int Costs<_Node>::ren (_Node* node1, _Node* node2) {
+  if (node1->get_label_id() == node2->get_label_id()) {
+    return 0;
+  }
+
+  return 1;
+}
+
+template<typename _Node>
+int Costs<_Node>::del (_Node node) {
+  return 1;
+}
+
+template<typename _Node>
+int Costs<_Node>::ins (_Node node) {
+  return 1;
+}
+
+template<typename _Node>
+int Costs<_Node>::ren () {
+  return 1;
+}
+
+template<typename _Node>
+int Costs<_Node>::del () {
+  return 1;
+}
+
+template<typename _Node>
+int Costs<_Node>::ins () {
+  return 1;
+}
+
+template<class _Node>
 struct Costs2 {
   // Basic rename cost function
   //
@@ -270,28 +354,57 @@ struct Costs2 {
   //          node2  The node having the desired name
   //
   // Return:  The cost of renaming node1 to node2
-  int ren (_node node1, _node node2) { return 2; }
+  int ren (_Node* node1, _Node* node2);
 
   // Basic delete cost function
   //
   // Params:  node The node to be deleted
   //
   // Return:  The cost of deleting node
-  int del (_node node) { return 2; }
+  int del (_Node node);
 
   // Basic insert cost function
   //
   // Params:  node The node to be inserted
   //
   // Return:  The cost of inserting node
-  int ins (_node node) { return 2; }
+  int ins (_Node node);
 
   // Dummy methods
-  int ren() { return 2; }
-  int del() { return 2; }
-  int ins() { return 2; }
+  int ren ();
+  int del ();
+  int ins ();
 };
 
+template<typename _Node>
+int Costs2<_Node>::ren (_Node* node1, _Node* node2) {
+  return 2;
+}
+
+template<typename _Node>
+int Costs2<_Node>::del (_Node node) {
+  return 2;
+}
+
+template<typename _Node>
+int Costs2<_Node>::ins (_Node node) {
+  return 2;
+}
+
+template<typename _Node>
+int Costs2<_Node>::ren () {
+  return 2;
+}
+
+template<typename _Node>
+int Costs2<_Node>::del () {
+  return 2;
+}
+
+template<typename _Node>
+int Costs2<_Node>::ins () {
+  return 2;
+}
 
 #endif // NODE_H
 
