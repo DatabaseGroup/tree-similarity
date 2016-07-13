36ddd66875cdbfa2ee6fd58f31838aca9bd4908d
Daniel Kocher <dkocher@cosy.sbg.ac.at>
2016-07-13 10:23:33 +0200

    Separate declaration and definition

--- src/upper_bound.h
+++ src/upper_bound.h
@@ -7,10 +7,10 @@
 class UpperBound {
 public:
   // Constructor(s)
-    UpperBound () { }
+  UpperBound ();
 
-  // Destructor(s)
-    ~UpperBound () { }
+  // Destructor
+  ~UpperBound ();
 
   // Computes a very simple upper bound by just adding the cost of deleting all
   // nodes of the left-hand-tree and the cost of insertion all nodes of the
@@ -20,14 +20,22 @@ public:
   //        optional cost-model
   //
   // Return: The simple upper bound for the two input-trees
-  template<class _node = Node, class _costs = Costs<_node>>
-  double compute_simple_upper_bound (_node* tree1, _node* tree2,
-    _costs costs = _costs())
-  {
-    return (tree1->get_subtree_size() * costs.del() +
-      tree2->get_subtree_size() * costs.ins()
-    );
-  }
+  template<class _Node = Node, class _Costs = Costs<_Node>>
+  double compute_simple_upper_bound (_Node* tree1, _Node* tree2,
+    _Costs costs = _Costs());
 };
+
+UpperBound::UpperBound () {}
+
+UpperBound::~UpperBound () {}
+
+template<typename _Node, typename _Costs>
+double UpperBound::compute_simple_upper_bound (_Node* tree1, _Node* tree2,
+  _Costs costs)
+{
+  return (tree1->get_subtree_size() * costs.del() +
+    tree2->get_subtree_size() * costs.ins()
+  );
+}
 
 #endif // UPPER_BOUND_H
