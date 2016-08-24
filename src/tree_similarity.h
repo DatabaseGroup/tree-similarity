54f43b7071fdd69d5b9c17ef1b080adc64436237
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2016-08-24 14:05:52 +0200

    Add single header file to include all header files

---
+++ src/tree_similarity.h
@@ -0,0 +1,26 @@
+#ifndef TREE_SIMILARITY_H
+#define TREE_SIMILARITY_H
+
+#include "common/common.h"
+
+#include "data_structures/k_heap.h"
+#include "data_structures/array_2d.h"
+
+#include "nodes/custom_node.h"
+#include "nodes/node.h"
+#include "nodes/string_node.h"
+
+#include "parser/parser.h"
+
+#include "sed/string_edit_distance.h"
+
+#include "tasm/ring_buffer.h"
+#include "tasm/tasm.h"
+
+#include "upper_bound/upper_bound.h"
+
+#include "wrappers/node_distance_pair.h"
+
+#include "zhang_shasha/zhang_shasha.h"
+
+#endif // TREE_SIMILARITY_H
