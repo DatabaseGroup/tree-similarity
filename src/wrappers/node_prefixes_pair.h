ab659871075fcb828868b88cf283460a592da55c
Daniel Kocher <dkocher@cosy.sbg.ac.at>
2016-08-24 18:49:13 +0200

    Implement NodePrefixesPair wrapper class

---
+++ src/wrappers/node_prefixes_pair.h
@@ -0,0 +1,64 @@
+#ifndef NODE_PREFIXES_PAIR_H
+#define NODE_PREFIXES_PAIR_H
+
+#include <vector>
+
+#include "../nodes/node.h"
+
+namespace wrappers {
+
+template<class _Node = nodes::Node>
+class NodePrefixesPair {
+private:
+  _Node root_;
+  std::vector<std::vector<_Node*>*> prefixes_;
+
+public:
+  NodePrefixesPair(_Node& root);
+  ~NodePrefixesPair();
+
+  void add_prefix(std::vector<_Node*>* subroot);
+  int get_number_of_prefixes() const;
+
+  const _Node get_root() const;
+  const std::vector<std::vector<_Node*>*> get_prefixes() const;
+
+  void set_root(_Node& root);
+};
+
+template<class _Node>
+NodePrefixesPair<_Node>::NodePrefixesPair(_Node& root) : root_(root) { }
+
+template<class _Node>
+NodePrefixesPair<_Node>::~NodePrefixesPair() {
+  prefixes_.clear();
+}
+
+template<class _Node>
+void NodePrefixesPair<_Node>::add_prefix(std::vector<_Node*>* subroot) {
+  prefixes_.push_back(subroot);
+}
+
+template<class _Node>
+int NodePrefixesPair<_Node>::get_number_of_prefixes() const {
+  return prefixes_.size();
+}
+
+template<class _Node>
+const _Node NodePrefixesPair<_Node>::get_root() const {
+  return root_;
+}
+
+template<class _Node>
+const std::vector<std::vector<_Node*>*> NodePrefixesPair<_Node>::get_prefixes() const {
+  return prefixes_;
+}
+
+template<class _Node>
+void NodePrefixesPair<_Node>::set_root(_Node& root) {
+  root_ = root;
+}
+
+} // namespace wrappers
+
+#endif // NODE_PREFIXES_PAIR_H
