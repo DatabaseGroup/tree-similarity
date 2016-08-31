c76441d0013f52d6db6c381c2a958a93d8d4c91f
TAuinger <auingerto@stud.sbg.ac.at>
2016-08-31 16:43:49 +0200

    ... also parser.h, this really should be the last

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -5,17 +5,18 @@ namespace parser {
 
 typedef std::unordered_map<std::string, int> LabelIDMap;
 
-nodes::Node* create_tree_from_string(char* str, LabelIDMap& hashtable,
+template<class _node = nodes::Node>
+_node* create_tree_from_string(char* str, LabelIDMap& hashtable,
   int& labelid)
 { 
   int length = std::strlen(str);
   int scope = -1;
    
-  std::vector<nodes::Node*> scope_parent_list;
-  std::vector<nodes::Node*>::const_iterator it;
+  std::vector<_node*> scope_parent_list;
+  typename std::vector<_node*>::const_iterator it;
   std::string label = "";
     
-  nodes::Node* root = new nodes::Node(1, 1);
+  _node* root = new _node(1, 1);
   for (int i = 0; i < length; i++) {
     if (i != 0 && scope <= -1) {
       break;
@@ -27,12 +28,12 @@ nodes::Node* create_tree_from_string(char* str, LabelIDMap& hashtable,
           hashtable.emplace(label, labelid++);
         }
         
-        nodes::Node* tmp_node = new nodes::Node(hashtable[label]);
+        _node* tmp_node = new _node(hashtable[label]);
         scope_parent_list.push_back(tmp_node);
         
         if (scope > 0) {
           it = scope_parent_list.begin() + scope_parent_list.size() - 2;
-          //nodes::Node* tmp = *it;
+          //_node* tmp = *it;
           //std::cout << label << ".parent_id:" << tmp->get_label_id() << " scope: " << scope << std::endl;
           //tmp->add_child(tmp_node);
           (*it)->add_child(tmp_node);
@@ -51,7 +52,7 @@ nodes::Node* create_tree_from_string(char* str, LabelIDMap& hashtable,
           hashtable.emplace(label, labelid++);
         }
 
-        nodes::Node* tmp_node = new nodes::Node(hashtable[label]);
+        _node* tmp_node = new _node(hashtable[label]);
         scope_parent_list.push_back(tmp_node);
         
         if (scope > 0 && scope_parent_list.size() > 1) {
