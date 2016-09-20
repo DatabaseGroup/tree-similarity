c57309ff2caa5dae5e27439a720506feb7d1a0dd
Daniel Kocher <dkocher@cosy.sbg.ac.at>
2016-09-20 17:21:47 +0200

    Add btree_test

--- src/tree_similarity.h
+++ src/tree_similarity.h
@@ -29,32 +29,53 @@
 
 #include "zhang_shasha/zhang_shasha.h"
 
+#include <chrono>
+#include <map>
 template<class _T, size_t _M>
 void test_btree(std::mt19937& mt) {
   data_structures::BTree<_T, _T, _M> btree;
-  //std::random_device rnd;
-  std::uniform_int_distribution<int> dist(1, 100);
+  std::map<_T, _T> log_map;
+
+  std::uniform_int_distribution<int> dist(1, 100000000);
   int random_number = 0;
   std::vector<int> random_numbers;
 
-  for (int i = 0; i < _M * _M * _M; ++i) {
+  while (random_numbers.size() < 50000) {
     random_number = dist(mt);
-    random_numbers.push_back(random_number);
-    try {
-      btree.insert(random_number, random_number);
-      btree.print();
-    } catch (std::exception e) {
-      std::cout << ">>> " << e.what() << " when inserting " << random_number << std::endl;
+
+    if (std::find(random_numbers.cbegin(), random_numbers.cend(), random_number) == random_numbers.cend()) {
+      random_numbers.push_back(random_number);
     }
   }
 
-  std::cout << "Insertion order: ";
-  for (int& random_number: random_numbers) {
-    std::cout << random_number << " ";
-  }
-  std::cout << std::endl;
+  std::cout << "Test run: " << random_numbers.size() << " random insertions"
+    << std::endl;
 
-  btree.print();
+  std::chrono::high_resolution_clock::time_point t1 =
+    std::chrono::high_resolution_clock::now();
+
+  for (int& random_number: random_numbers) {
+    log_map.insert(std::make_pair(random_number, random_number));
+  }
+
+  std::chrono::high_resolution_clock::time_point t2 =
+    std::chrono::high_resolution_clock::now();
+
+  std::cout << "std::map: "
+    << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()
+    << std::endl;   
+
+  t1 = std::chrono::high_resolution_clock::now();
+  
+  for (int& random_number: random_numbers) {
+    btree.insert(random_number, random_number);
+  }
+
+  t2 = std::chrono::high_resolution_clock::now();
+ 
+  std::cout << "data_structures::BTree: "
+    << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()
+    << std::endl;
 }
 
 #endif // TREE_SIMILARITY_H
