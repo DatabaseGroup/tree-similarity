ae522f86b82febaab149a895a942a959aef67ed6
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2016-09-27 17:34:23 +0200

    Implement naive_search until filter_and_add call

--- src/structure_search/structure_search.h
+++ src/structure_search/structure_search.h
@@ -12,11 +12,20 @@
 namespace structure_search {
 
 template<class _NodeData>
+void filter_and_add(nodes::Node<_NodeData>* query,
+  std::multiset<_NodeData>& labels, std::multiset<_NodeData>& labels_query,
+  data_structures::DeweyIdentifier& dewey_id, int& m,
+  data_structures::KHeap<nodes::Node<_NodeData>>& results)
+{
+  
+}
+
+template<class _NodeData>
 data_structures::KHeap<nodes::Node<_NodeData>> naive_search(
   nodes::Node<_NodeData>* query, std::vector<nodes::Node<_NodeData>*> trees,
   int& m, const int& k,
   data_structures::BTree<_NodeData, std::list<data_structures::DeweyIdentifier>>& label_index,
-  std::set<_NodeData>& labels_query)
+  std::multiset<_NodeData>& labels_query)
 {
   data_structures::PostingListContainer<_NodeData> pl;
 
@@ -33,13 +42,26 @@ data_structures::KHeap<nodes::Node<_NodeData>> naive_search(
   std::stack<std::pair<data_structures::DeweyIdentifier, std::multiset<_NodeData>>> s;
   data_structures::KHeap<nodes::Node<_NodeData>> results(6);
 
+  std::pair<data_structures::DeweyIdentifier, std::multiset<_NodeData>> previous;
   while (!pl.empty()) {
     auto p = pl.next();
     std::multiset<_NodeData> labels;
 
-    //while (!s.empty() && !s.top().first.is_prefix(p.first)) {
+    _NodeData& current_label = p.second;
+    data_structures::DeweyIdentifier& current_dewey_id = p.first;
 
-    //}
+    while (!s.empty() && !(previous = s.top()).first.is_prefix(p.first)) {
+      s.pop();
+
+      // TODO: is this the best/fastet way to build the union of 2 multisets
+      // or rather use std::set_union ...
+      // O(n log (n + k)), n = distance between the two multisets,
+      // k = size of the calling multiset (labels)
+      labels.insert(previous.second.begin(), previous.second.end());
+
+      // no return value, m is modified since it's passed by reference
+      filter_and_add(query, labels, labels_query, previous.first, m, results);
+    }
   }
 
   return results;
