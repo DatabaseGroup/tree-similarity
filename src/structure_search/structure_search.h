4c8b75c4370b0ee28ddfe40f07e45e20b6b05d43
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2016-09-27 19:08:00 +0200

    Implement filter_and_add (complete)

--- src/structure_search/structure_search.h
+++ src/structure_search/structure_search.h
@@ -10,6 +10,7 @@
 #include "../data_structures/k_heap.h"
 #include "../data_structures/posting_list_container.h"
 #include "../wrappers/node_index_value.h"
+#include "../zhang_shasha/zhang_shasha.h"
 
 namespace structure_search {
 
@@ -17,7 +18,7 @@ template<class _NodeData>
 void filter_and_add(nodes::Node<_NodeData>* query,
   std::multiset<_NodeData>& labels, std::multiset<_NodeData>& labels_query,
   data_structures::DeweyIdentifier& dewey_id, int& m,
-  data_structures::KHeap<nodes::Node<_NodeData>>& results,
+  data_structures::KHeap<std::pair<int, nodes::Node<_NodeData>*>>& results,
   data_structures::BTree<data_structures::DeweyIdentifier, wrappers::NodeIndexValue<_NodeData>>& node_index)
 {
   std::multiset<_NodeData> intersection;
@@ -25,12 +26,35 @@ void filter_and_add(nodes::Node<_NodeData>* query,
     labels_query.end(), std::inserter(intersection, intersection.begin())
   );
 
+  wrappers::NodeIndexValue<_NodeData> node_information =
+    node_index.search(dewey_id).second.second;
   size_t intersection_size = intersection.size();
-  //size_t label_diff = std::max(( - intersection_size), ( - intersection_size));
+  int label_diff = std::max((labels_query.size() - intersection_size),
+    (node_information.size() - intersection_size)
+  );
+  // TODO: replace 3 by depth of the query
+  int depth_diff = static_cast<int>(std::abs(3 - node_information.depth()));
+
+  if ((label_diff <= m) && (depth_diff <= m)) {
+    // TODO: replace template parameters with actual correct ones (custom costs)
+    int edit_distance = static_cast<int>(
+      zhang_shasha::compute_zhang_shasha<_NodeData, nodes::StringCosts<_NodeData>>(
+        query, node_information.root()
+      )
+    );
+
+    if ((edit_distance < m) || ((edit_distance == m) && !results.full())) {
+      results.insert(std::make_pair(edit_distance, node_information.root()));
+    }
+  }
+
+  if (results.full()) {
+    m = results.front().first;
+  }
 }
 
 template<class _NodeData>
-data_structures::KHeap<nodes::Node<_NodeData>> naive_search(
+data_structures::KHeap<std::pair<int, nodes::Node<_NodeData>*>> naive_search(
   nodes::Node<_NodeData>* query, std::vector<nodes::Node<_NodeData>*> trees,
   int& m, const int& k,
   data_structures::BTree<_NodeData, std::list<data_structures::DeweyIdentifier>>& label_index,
@@ -50,7 +74,7 @@ data_structures::KHeap<nodes::Node<_NodeData>> naive_search(
 
   std::cout << pl.size() << std::endl;
   std::stack<std::pair<data_structures::DeweyIdentifier, std::multiset<_NodeData>>> s;
-  data_structures::KHeap<nodes::Node<_NodeData>> results(6);
+  data_structures::KHeap<std::pair<int, nodes::Node<_NodeData>*>> results(6);
 
   std::pair<data_structures::DeweyIdentifier, std::multiset<_NodeData>> previous;
   while (!pl.empty()) {
