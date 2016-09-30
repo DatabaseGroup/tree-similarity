ca39ea8275bd72bff6b180ef719050511cc0b6b7
Daniel Kocher <dkocher@cosy.sbg.ac.at>
2016-09-30 22:26:22 +0200

    Add --tasm and --ss-naive command-line options and corresponding test functions

--- src/tree_similarity.cc
+++ src/tree_similarity.cc
@@ -7,10 +7,14 @@
 
 #include "tree_similarity.h"
 
+void test_tasm();
+void test_ss_naive();
+
 // TODO: tobi: put everything in a method e.g. called get_sbs_fs(node* t1, node* t2)
 //
 int main (int argc, char* argv[]) {
-  if(argc != 3 && argc != 4 && argc != 5 && argc != 6) {
+  //if(argc != 3 && argc != 4 && argc != 5 && argc != 6) {
+  if (argc <= 1) {
     std::cout << "TODO print a help message" << std::endl;
     return 0;
   }
@@ -18,7 +22,7 @@ int main (int argc, char* argv[]) {
   // a stack overflow because argv and argc are passed like normal function
   // parameters via the stack - get the maximum argument length under linux via
   // $ getconf ARG_MAX
-  if(argv[1] == std::string("--apted")){ // option to test apted
+  if (argv[1] == std::string("--apted")) { // option to test apted
     // variables needed for parser functions to create trees
     parser::LabelIDMap hashtable_label_to_id;
     int node_id_counter = 1;
@@ -42,6 +46,12 @@ int main (int argc, char* argv[]) {
     delete test_tree2;
 
     return 0; //terminate programm
+  } else if (argv[1] == std::string("--tasm")) {
+    test_tasm();
+    return 0;
+  } else if (argv[1] == std::string("--ss-naive")) {
+    test_ss_naive();
+    return 0;
   }
 /*
   std::cout << argv[1] << " " << argv[2] << std::endl;
@@ -70,7 +80,6 @@ int main (int argc, char* argv[]) {
   // no need to generate basic cost model since it is set as default template
   // parameter
 
-  
   nodes::Node<nodes::StringNodeData>* a1 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("a"));
   nodes::Node<nodes::StringNodeData>* r1 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("r"));
   nodes::Node<nodes::StringNodeData>* d1 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("d"));
@@ -162,8 +171,11 @@ int main (int argc, char* argv[]) {
   delete tree2_postorder;
 */
 
-/*
-  // TEST CASES OF DANIEL, DO NOT TOUCH THESE! ;)
+  return 0;
+}
+
+void test_tasm() {
+  // TASM TEST CASES (DANIEL), DO NOT TOUCH THESE! ;)
   //
   nodes::Node<nodes::StringNodeData>* d1 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("John"));
   nodes::Node<nodes::StringNodeData>* d2 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("auth"));
@@ -241,7 +253,6 @@ int main (int argc, char* argv[]) {
   postorder_queue.push(*d21);
   postorder_queue.push(*d22);
 
-
   std::vector<nodes::Node<nodes::StringNodeData>> result = tasm::prb_pruning<nodes::StringNodeData>(postorder_queue, 6);
 
   std::cout << "prb-pruning result:" << std::endl;
@@ -315,67 +326,179 @@ int main (int argc, char* argv[]) {
     std::cout << rit->get_node().get_data()->get_label() << ", " << rit->get_distance() <<" | ";
   }
   std::cout << std::endl;
-*/
+}
 
-  /*
-  data_structures::BTree<int, std::string, 6> btree_even_m;
+void test_ss_naive() {
+  // STRUCTURE SEARCH TEST CASES (DANIEL), DO NOT TOUCH THESE! ;)
+  //
+  nodes::Node<nodes::StringNodeData>* t1 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("dblp"));
+  nodes::Node<nodes::StringNodeData>* t2 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("inproceedings"));
+  nodes::Node<nodes::StringNodeData>* t3 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("inproceedings"));
+  nodes::Node<nodes::StringNodeData>* t4 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("inproceedings"));
+  nodes::Node<nodes::StringNodeData>* t5 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("inproceedings"));
+  nodes::Node<nodes::StringNodeData>* t6 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("author"));
+  nodes::Node<nodes::StringNodeData>* t7 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("author"));
+  nodes::Node<nodes::StringNodeData>* t8 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("author"));
+  nodes::Node<nodes::StringNodeData>* t9 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("author"));
+  nodes::Node<nodes::StringNodeData>* t10 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("author"));
+  nodes::Node<nodes::StringNodeData>* t11 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("title"));
+  nodes::Node<nodes::StringNodeData>* t12 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("title"));
+  nodes::Node<nodes::StringNodeData>* t13 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("title"));
+  nodes::Node<nodes::StringNodeData>* t14 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("title"));
+  nodes::Node<nodes::StringNodeData>* t15 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Hanrahn"));
+  nodes::Node<nodes::StringNodeData>* t16 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Analytic database ..."));
+  nodes::Node<nodes::StringNodeData>* t17 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Vahdat"));
+  nodes::Node<nodes::StringNodeData>* t18 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Symbiosis in scale out ..."));
+  nodes::Node<nodes::StringNodeData>* t19 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Hamilton"));
+  nodes::Node<nodes::StringNodeData>* t20 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Internet scale ..."));
+  nodes::Node<nodes::StringNodeData>* t21 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Meliou"));
+  nodes::Node<nodes::StringNodeData>* t22 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Suciu"));
+  nodes::Node<nodes::StringNodeData>* t23 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Tiresias ..."));
 
-  btree_even_m.insert(1, "1"); btree_even_m.print();
-  btree_even_m.insert(3, "3"); btree_even_m.print();
-  btree_even_m.insert(5, "5"); btree_even_m.print();
-  btree_even_m.insert(7, "7"); btree_even_m.print();
-  btree_even_m.insert(99, "99"); btree_even_m.print();
-  btree_even_m.insert(9, "9"); btree_even_m.print();
+  nodes::Node<nodes::StringNodeData>* q1 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("inproceedings"));
+  nodes::Node<nodes::StringNodeData>* q2 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("author"));
+  nodes::Node<nodes::StringNodeData>* q3 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("title"));
+  nodes::Node<nodes::StringNodeData>* q4 = new nodes::Node<nodes::StringNodeData>(new nodes::StringNodeData("Hanrahn"));
 
-  btree_even_m.insert(8, "8"); btree_even_m.print();
-  btree_even_m.insert(2, "2"); btree_even_m.print();
-  btree_even_m.insert(4, "4"); btree_even_m.print();
-  btree_even_m.insert(6, "6"); btree_even_m.print();
-  btree_even_m.insert(10, "10"); btree_even_m.print();
-  btree_even_m.insert(11, "11"); btree_even_m.print();
-  btree_even_m.insert(12, "12"); btree_even_m.print();
-  btree_even_m.insert(13, "13"); btree_even_m.print();
-  btree_even_m.insert(14, "14"); btree_even_m.print();
-  btree_even_m.insert(15, "15"); btree_even_m.print();
-  btree_even_m.insert(16, "16"); btree_even_m.print();
-  btree_even_m.insert(17, "17"); btree_even_m.print();
-  btree_even_m.insert(18, "18"); btree_even_m.print();
-  btree_even_m.insert(19, "19"); btree_even_m.print();
-  btree_even_m.insert(20, "20"); btree_even_m.print();
-  btree_even_m.insert(21, "21"); btree_even_m.print();
-  btree_even_m.insert(22, "22"); btree_even_m.print();
-  btree_even_m.insert(23, "23"); btree_even_m.print();
-  btree_even_m.insert(24, "24"); btree_even_m.print();
-  btree_even_m.insert(25, "25"); btree_even_m.print();
-  btree_even_m.insert(26, "26"); btree_even_m.print();
-  btree_even_m.insert(27, "27"); btree_even_m.print();
-  btree_even_m.insert(28, "28"); btree_even_m.print();
-  btree_even_m.insert(29, "29"); btree_even_m.print();
-  btree_even_m.insert(17, "42"); btree_even_m.print();
-  btree_even_m.insert(30, "30"); btree_even_m.print();
-  btree_even_m.insert(31, "31"); btree_even_m.print();
-  btree_even_m.insert(32, "32"); btree_even_m.print();
-  btree_even_m.insert(33, "33"); btree_even_m.print();
+  t1->add_child(t2); t1->add_child(t3); t1->add_child(t4); t1->add_child(t5);
+  t2->add_child(t6); t2->add_child(t11);
+  t3->add_child(t7); t3->add_child(t12);
+  t4->add_child(t8); t4->add_child(t13);
+  t5->add_child(t9); t5->add_child(t10); t5->add_child(t14);
+  t6->add_child(t15);
+  t7->add_child(t17);
+  t8->add_child(t19);
+  t9->add_child(t21);
+  t10->add_child(t22);
+  t11->add_child(t16);
+  t12->add_child(t18);
+  t13->add_child(t20);
+  t14->add_child(t23);
 
-  data_structures::BTree<int, std::string, 5> btree_odd_m;
-  btree_odd_m.insert(1, "One"); btree_odd_m.print();
-  btree_odd_m.insert(3, "Three"); btree_odd_m.print();
-  btree_odd_m.insert(5, "Five"); btree_odd_m.print();
-  btree_odd_m.insert(7, "Seven"); btree_odd_m.print();
-  btree_odd_m.insert(99, "Ninety-Nine"); btree_odd_m.print();
-  btree_odd_m.insert(8, "Eight"); btree_odd_m.print();
-  btree_odd_m.insert(9, "Nine"); btree_odd_m.print();
-  btree_odd_m.insert(11, "Eleven"); btree_odd_m.print();
-  btree_odd_m.insert(12, "Twelve"); btree_odd_m.print();
+  q1->add_child(q2); q1->add_child(q3);
+  q2->add_child(q4);
 
-  int runs = 10;
-  int i = 0;
-  std::random_device rnd;
-  std::mt19937 mt(rnd()); // for reproduction: seed 42
-  const int m = 16;
+  std::vector<nodes::Node<nodes::StringNodeData>*> trees = { t1 };
 
-  test_btree<int, m>(mt);
-*/
+  data_structures::BTree<nodes::StringNodeData, std::list<data_structures::DeweyIdentifier>> label_index;
+  data_structures::BTree<data_structures::DeweyIdentifier, wrappers::NodeIndexValue<nodes::StringNodeData>> node_index;
+  
+  data_structures::DeweyIdentifier dblp(1);
+  data_structures::DeweyIdentifier inproceedings1(dblp, 0);
+  data_structures::DeweyIdentifier inproceedings2(dblp, 1);
+  data_structures::DeweyIdentifier inproceedings3(dblp, 2);
+  data_structures::DeweyIdentifier inproceedings4(dblp, 3);
+  data_structures::DeweyIdentifier author1(inproceedings1, 0);
+  data_structures::DeweyIdentifier author2(inproceedings2, 0);
+  data_structures::DeweyIdentifier author3(inproceedings3, 0);
+  data_structures::DeweyIdentifier author4(inproceedings4, 0);
+  data_structures::DeweyIdentifier author5(inproceedings4, 1);
+  data_structures::DeweyIdentifier title1(inproceedings1, 1);
+  data_structures::DeweyIdentifier title2(inproceedings2, 1);
+  data_structures::DeweyIdentifier title3(inproceedings3, 1);
+  data_structures::DeweyIdentifier title4(inproceedings4, 2);
+  data_structures::DeweyIdentifier hanrahn(author1, 0);
+  data_structures::DeweyIdentifier analytic_db(title1, 0);
+  data_structures::DeweyIdentifier vahdat(author2, 0);
+  data_structures::DeweyIdentifier symbiosis_in_scale_out(title2, 0);
+  data_structures::DeweyIdentifier hamilton(author3, 0);
+  data_structures::DeweyIdentifier internet_scale(title3, 0);
+  data_structures::DeweyIdentifier meliou(author4, 0);
+  data_structures::DeweyIdentifier suciu(author5, 0);
+  data_structures::DeweyIdentifier tiresias(title4, 0);
 
-  return 0;
+  label_index.insert(nodes::StringNodeData("dblp"), { dblp });
+  label_index.insert(nodes::StringNodeData("inproceedings"), { inproceedings1,
+    inproceedings2, inproceedings3, inproceedings4  }
+  );
+  label_index.insert(nodes::StringNodeData("author"), { author1, author2, author3, author4, author5 });
+  label_index.insert(nodes::StringNodeData("title"), { title1, title2, title3, title4 });
+  label_index.insert(nodes::StringNodeData("Hanrahn"), { hanrahn });
+  label_index.insert(nodes::StringNodeData("Analytic database ..."), { analytic_db });
+  label_index.insert(nodes::StringNodeData("Vahdat"), { vahdat });
+  label_index.insert(nodes::StringNodeData("Symbiosis in scale out ..."), { symbiosis_in_scale_out });
+  label_index.insert(nodes::StringNodeData("Hamilton"), { hamilton });
+  label_index.insert(nodes::StringNodeData("Internet scale ..."), { internet_scale });
+  label_index.insert(nodes::StringNodeData("Meliou"), { meliou });
+  label_index.insert(nodes::StringNodeData("Suciu"), { suciu });
+  label_index.insert(nodes::StringNodeData("Tiresias ..."), { tiresias });
+
+  node_index.insert(dblp, wrappers::NodeIndexValue<nodes::StringNodeData>(23, 4, nodes::StringNodeData("dblp"), t1));
+  node_index.insert(inproceedings1, wrappers::NodeIndexValue<nodes::StringNodeData>(5, 3, nodes::StringNodeData("inproceedings"), t2));
+  node_index.insert(inproceedings2, wrappers::NodeIndexValue<nodes::StringNodeData>(5, 3, nodes::StringNodeData("inproceedings"), t3));
+  node_index.insert(inproceedings3, wrappers::NodeIndexValue<nodes::StringNodeData>(5, 3, nodes::StringNodeData("inproceedings"), t4));
+  node_index.insert(inproceedings4, wrappers::NodeIndexValue<nodes::StringNodeData>(7, 3, nodes::StringNodeData("inproceedings"), t5));
+  node_index.insert(author1, wrappers::NodeIndexValue<nodes::StringNodeData>(2, 2, nodes::StringNodeData("author"), t6));
+  node_index.insert(author2, wrappers::NodeIndexValue<nodes::StringNodeData>(2, 2, nodes::StringNodeData("author"), t7));
+  node_index.insert(author3, wrappers::NodeIndexValue<nodes::StringNodeData>(2, 2, nodes::StringNodeData("author"), t8));
+  node_index.insert(author4, wrappers::NodeIndexValue<nodes::StringNodeData>(2, 2, nodes::StringNodeData("author"), t9));
+  node_index.insert(author5, wrappers::NodeIndexValue<nodes::StringNodeData>(2, 2, nodes::StringNodeData("author"), t10));
+  node_index.insert(title1, wrappers::NodeIndexValue<nodes::StringNodeData>(2, 2, nodes::StringNodeData("title"), t11));
+  node_index.insert(title2, wrappers::NodeIndexValue<nodes::StringNodeData>(2, 2, nodes::StringNodeData("title"), t12));
+  node_index.insert(title3, wrappers::NodeIndexValue<nodes::StringNodeData>(2, 2, nodes::StringNodeData("title"), t13));
+  node_index.insert(title4, wrappers::NodeIndexValue<nodes::StringNodeData>(2, 2, nodes::StringNodeData("title"), t14));
+  node_index.insert(hanrahn, wrappers::NodeIndexValue<nodes::StringNodeData>(1, 1, nodes::StringNodeData("hanrahn"), t15));
+  node_index.insert(analytic_db, wrappers::NodeIndexValue<nodes::StringNodeData>(1, 1, nodes::StringNodeData("analytic_db"), t16));
+  node_index.insert(vahdat, wrappers::NodeIndexValue<nodes::StringNodeData>(1, 1, nodes::StringNodeData("vahdat"), t17));
+  node_index.insert(symbiosis_in_scale_out, wrappers::NodeIndexValue<nodes::StringNodeData>(1, 1, nodes::StringNodeData("symbiosis_in_scale_out"), t18));
+  node_index.insert(hamilton, wrappers::NodeIndexValue<nodes::StringNodeData>(1, 1, nodes::StringNodeData("hamilton"), t19));
+  node_index.insert(internet_scale, wrappers::NodeIndexValue<nodes::StringNodeData>(1, 1, nodes::StringNodeData("internet_scale"), t20));
+  node_index.insert(meliou, wrappers::NodeIndexValue<nodes::StringNodeData>(1, 1, nodes::StringNodeData("meliou"), t21));
+  node_index.insert(suciu, wrappers::NodeIndexValue<nodes::StringNodeData>(1, 1, nodes::StringNodeData("suciu"), t22));
+  node_index.insert(tiresias, wrappers::NodeIndexValue<nodes::StringNodeData>(1, 1, nodes::StringNodeData("tiresias"), t23));
+
+  std::multiset<nodes::StringNodeData> labels_query = {
+    nodes::StringNodeData("inproceedings"), nodes::StringNodeData("author"),
+    nodes::StringNodeData("title"), nodes::StringNodeData("Hanrahn")
+  };
+  
+  int m = 4;
+  const int k = 6;
+  
+  data_structures::KHeap<wrappers::NodeDistancePair<nodes::StringNodeData>> ranking =
+    structure_search::naive_search<nodes::StringNodeData>(q1, trees, m, k,
+      label_index, node_index, labels_query
+    );
+
+  std::cout << "ranking: " << ranking.size() << " / " << ranking.max_size() << std::endl;
+  std::cout << ranking.max_size() << "-Heap: | ";
+  for (auto rit = ranking.rbegin(); rit != ranking.rend(); --rit) {
+    std::cout << rit->get_node().get_data()->get_label() << ", " << rit->get_distance() <<" | ";
+  }
+  std::cout << std::endl;
+
+  std::cout << std::endl << std::endl;
+
+  std::queue<nodes::Node<nodes::StringNodeData>> po_queue;
+  po_queue.push(*t15);
+  po_queue.push(*t6);
+  po_queue.push(*t16);
+  po_queue.push(*t11);
+  po_queue.push(*t2);
+  po_queue.push(*t17);
+  po_queue.push(*t7);
+  po_queue.push(*t18);
+  po_queue.push(*t12);
+  po_queue.push(*t3);
+  po_queue.push(*t19);
+  po_queue.push(*t8);
+  po_queue.push(*t20);
+  po_queue.push(*t13);
+  po_queue.push(*t4);
+  po_queue.push(*t21);
+  po_queue.push(*t9);
+  po_queue.push(*t22);
+  po_queue.push(*t10);
+  po_queue.push(*t23);
+  po_queue.push(*t14);
+  po_queue.push(*t5);
+  po_queue.push(*t1);
+
+  auto heap = tasm::tasm_postorder<nodes::StringNodeData, nodes::StringCosts<nodes::StringNodeData>>(*q1, po_queue, k);
+  std::cout << "ranking: " << heap.size() << " / " << heap.max_size() << std::endl;
+  std::cout << heap.max_size() << "-Heap: | ";
+  for (auto rit = heap.rbegin(); rit != heap.rend(); --rit) {
+    std::cout << rit->get_node().get_data()->get_label() << ", " << rit->get_distance() <<" | ";
+  }
+  std::cout << std::endl;
 }
