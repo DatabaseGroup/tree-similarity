c65c1806803e18ff3afe65e4b639e65838fd8139
Mateusz Pawlik <mpawlik@cosy.sbg.ac.at>
2017-06-14 17:56:31 +0200

    Added execution of Zhang and Shasha.

--- src/tree_similarity.cc
+++ src/tree_similarity.cc
@@ -1,20 +1,20 @@
 // The MIT License (MIT)
 // Copyright (c) 2017 Mateusz Pawlik, Nikolaus Augsten, and Daniel Kocher.
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights 
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-// copies of the Software, and to permit persons to whom the Software is 
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
@@ -33,7 +33,7 @@ template<class _Label> using CostModel = cost_model::UnitCostModel<_Label>;
 int main(int argc, char** argv) {
     // TODO: using Parser = ;
     using Label = label::StringLabel;
-    
+
 
     node::Node<Label> root(Label("abc"));
     node::Node<Label> root2(Label("abcd"));
@@ -82,6 +82,8 @@ int main(int argc, char** argv) {
     std::cout << "child1: " << child1.children_number() << std::endl;
     std::cout << "child2: " << child2.children_number() << std::endl;
     std::cout << "child3: " << child3.children_number() << std::endl;
+
+    std::cout << zhang_shasha::zhang_shasha_ted(root, root, cost_model) << std::endl;
 
     return 0;
 }
