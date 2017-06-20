dda08c7edba866b0eca749b91d0d741a6ab023e8
Mateusz Pawlik <mpawlik@cosy.sbg.ac.at>
2017-06-20 09:53:20 +0200

    unit_cost_model: Added const to cost functions.

--- src/cost_model/unit_cost_model.h
+++ src/cost_model/unit_cost_model.h
@@ -1,20 +1,20 @@
 // The MIT License (MIT)
 // Copyright (c) 2017 Mateusz Pawlik, Nikolaus Augsten, and Daniel Kocher.
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights 
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-// copies of the Software, and to permit persons to whom the Software is 
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
@@ -49,19 +49,19 @@ struct UnitCostModel {
     /// \param node1 The node to be renamed.
     /// \param node2  The node having the desired name.
     /// \return Integer cost of renaming node1 to node2.
-    int ren(const node::Node<Label>& node1, const node::Node<Label>& node2);
+    int ren(const node::Node<Label>& node1, const node::Node<Label>& node2) const;
 
     /// Basic delete cost function.
     ///
     /// \param node The node to be deleted.
     /// \return Integer cost of deleting node.
-    int del(const node::Node<Label>& node);
+    int del(const node::Node<Label>& node) const;
 
     /// Basic insert cost function.
     ///
     /// \param node The node to be inserted.
     /// \return Integer cost of inserting node.
-    int ins(const node::Node<Label>& node);
+    int ins(const node::Node<Label>& node) const;
 };
 
 // Implementational details
--- src/cost_model/unit_cost_model_impl.h
+++ src/cost_model/unit_cost_model_impl.h
@@ -1,20 +1,20 @@
 // The MIT License (MIT)
 // Copyright (c) 2017 Mateusz Pawlik, Nikolaus Augsten, and Daniel Kocher.
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights 
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-// copies of the Software, and to permit persons to whom the Software is 
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
@@ -28,23 +28,21 @@
 #define TREE_SIMILARITY_COST_MODEL_UNIT_COST_MODEL_IMPL_H
 
 template <class Label>
-int UnitCostModel<Label>::ren(
-    const node::Node<Label>& node1,
-    const node::Node<Label>& node2) {
+int UnitCostModel<Label>::ren(const node::Node<Label>& node1,
+                              const node::Node<Label>& node2) const {
   if (node1.label() == node2.label()) {
     return 0;
   }
-
   return 1;
 }
 
 template <class Label>
-int UnitCostModel<Label>::del(const node::Node<Label>& node) {
+int UnitCostModel<Label>::del(const node::Node<Label>& node) const {
   return 1;
 }
 
 template <class Label>
-int UnitCostModel<Label>::ins(const node::Node<Label>& node) {
+int UnitCostModel<Label>::ins(const node::Node<Label>& node) const {
   return 1;
 }
 
