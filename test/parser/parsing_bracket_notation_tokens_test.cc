11bcaa75dd67146424dee43d8d25ec2d6c6c5ded
Mateusz Pawlik <mpawlik@cosy.sbg.ac.at>
2017-07-10 14:04:19 +0200

    test/parser: Added bracket notation tokenizer correctness test.

---
+++ test/parser/parsing_bracket_notation_tokens_test.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "bracket_notation_parser.h"
+
+/// Convert vector of int values to is string representation.
+///
+/// TODO: Move this method to some util.
+///
+/// \param v Vector of int values.
+/// \return String representation of v.
+const std::string vector_to_string(const std::vector<std::string>& v) {
+  std::string s;
+  for (auto e : v) {
+    s += e + ",";
+  }
+  s.pop_back(); // Delete the last coma
+  return s;
+}
+
+int main() {
+
+  std::cout << "Tokens test." << std::endl;
+
+  // Parse test cases from file.
+  std::ifstream test_cases_file("tokens_test_data.txt");
+
+  if (!test_cases_file) {
+    std::cerr << "ERROR: Problem reading the test file." << std::endl;
+  }
+
+  for (std::string line; std::getline( test_cases_file, line);) {
+    if (line[0] == '#') {
+      std::getline(test_cases_file, line);
+      std::string input_tree = line;
+      std::getline(test_cases_file, line);
+      std::string correct_result = line;
+
+      // Parse test tree.
+      parser::BracketNotationParser bnp;
+      std::string computed_results = vector_to_string(bnp.get_tokens(input_tree));
+
+      if (correct_result != computed_results) {
+        std::cerr << "Incorrect tokens: " << computed_results << " instead of " << correct_result << std::endl;
+        std::cerr << input_tree << std::endl;
+        return -1;
+      }
+    }
+  }
+
+  return 0;
+}
