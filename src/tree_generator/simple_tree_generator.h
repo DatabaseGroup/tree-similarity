a5312a5821af04023e156c270174606938e19d50
Mateusz Pawlik <mpawlik@.cosy.sbg.ac.at>
2018-01-11 14:48:01 +0100

    tree_generator: Documenting.

--- src/tree_generator/simple_tree_generator.h
+++ src/tree_generator/simple_tree_generator.h
@@ -26,6 +26,9 @@
 /// It generates a correct bracket notation representation of a tree of
 /// a specific given size.
 ///
+/// Additionally, the generated trees can be modified by given number of edits.
+/// Random nodes can be deleted or renamed. Insertion is not implemented.
+///
 /// Based on https://frosch.cosy.sbg.ac.at/mpawlik/tree-generator
 
 #ifndef TREE_GENERATOR_SIMPLE_TREE_GENERATOR_H
@@ -42,13 +45,39 @@ namespace tree_generator {
 class SimpleTreeGenerator {
 public:
   SimpleTreeGenerator();
+  /// Generates a random tree with number of nodes equal to tree_size.
+  /// NOTE: The shapes of generated trees are more to the right.
+  ///
+  /// \param tree_size Number of nodes in the generated tree.
+  /// \return A tree in bracket notation.
   std::string generate_tree(unsigned int tree_size);
+  /// Generates a random alphabet of labels.
+  ///
+  /// \param tree_size Number of nodes in the generated tree.
+  /// \param label_length Number of characters in the labels.
+  /// \param ratio = |alphabet| / tree_size.
+  /// \return Vector of random string labels.
   std::vector<std::string> generate_random_alphabet(unsigned int tree_size,
       unsigned int label_length, double ratio);
-  std::string modify_tree(std::string& tree_string, int stree_size, int num_edits);
+  /// Modifies a tree passed in bracket notation with the given number of edit
+  /// operations.
+  /// Currently, rename and deletion is implemented.
+  ///
+  /// NOTE: The original tree is not modified.
+  /// NOTE: A rename may not rename a node if the same label is chosen.
+  /// NOTE: A deletion may delete a node that has been renamed that results
+  ///       in one edit operation.
+  ///
+  /// \param tree_string Tree to modify in the bracket notation.
+  /// \param tree_size Number of nodes in the tree.
+  /// \param num_edits Number of edit operations to apply.
+  /// \return A modified tree in bracket notation.
+  std::string modify_tree(std::string& tree_string, int tree_size, int num_edits);
 private:
+  /// Structure brackets for representing nodes relationships.
   const std::string kLeftBracket = "{";
   const std::string kRightBracket = "}";
+  /// List of labels to choose from for genrating trees.
   std::vector<std::string> alphabet_;
 };
 
