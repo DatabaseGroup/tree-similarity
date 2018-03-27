66f7b68d6a41784b2b7fb8819d544aa26e4e8cd2
Mateusz Pawlik <mpawlik@.cosy.sbg.ac.at>
2018-03-27 15:34:34 +0200

    touzet: Upgraded framework for testing ted for different versions of touzet.

--- test/touzet/touzet_ted_test.cc
+++ test/touzet/touzet_ted_test.cc
@@ -14,177 +14,314 @@
 #include "simple_tree_generator.h"
 #include "zhang_shasha.h"
 
-int main() {
+struct TestParams {
+  std::string input_file;
+  bool verify_expected;
+  bool set_zs_k;
+  bool alg_touzet_ted;
+  bool alg_touzet_ted_depth_pruning;
+  bool alg_touzet_ted_kr_loop;
+  bool alg_touzet_ted_kr_set;
+  bool alg_zs_ted;
+  bool include_random;
+  bool lower_than_threshold;
+  
+  TestParams() {
+    input_file = "";
+    verify_expected = false;
+    set_zs_k = false;
+    alg_touzet_ted = false;
+    alg_touzet_ted_depth_pruning = false;
+    alg_touzet_ted_kr_loop = false;
+    alg_touzet_ted_kr_set = false;
+    alg_zs_ted = false;
+    include_random = false;
+    lower_than_threshold = false;
+  };
+  
+  void set_default() {
+    input_file = "touzet_ted_test_data.txt";
+    verify_expected = true;
+    set_zs_k = false;
+    alg_touzet_ted = false;
+    alg_touzet_ted_depth_pruning = false;
+    alg_touzet_ted_kr_loop = false;
+    alg_touzet_ted_kr_set = true;
+    alg_zs_ted = false;
+    include_random = false;
+    lower_than_threshold = false;
+  };
+};
 
+struct TestInput {
+  std::string description;
+  std::string t1;
+  std::string t2;
+  int k;
+  double expected_result; // May be empty if not used in a test.
+};
+
+void communicate_result_error(double computed, double expected) {
+  std::cerr << "ERROR: Incorrect TED result " << computed << " instead of " << expected << std::endl;
+};
+
+int compare_results(double computed, double expected, TestParams& tp) {
+  if (tp.lower_than_threshold) {
+    if (computed > expected) {
+      communicate_result_error(computed, expected);
+      return -1;
+    }
+  } else if (computed != expected) {
+    communicate_result_error(computed, expected);
+    return -1;
+  }
+  return 0;
+}
+
+int single_test_case(TestParams& tp, TestInput& ti) {
   using Label = label::StringLabel;
   using CostModel = cost_model::UnitCostModel<Label>;
+  
+  std::cout << "-------------------------------------------------" << std::endl;
+  std::cout << "DESCRIPTION" << std::endl;
+  std::cout << ti.description << std::endl;
+  
+  // Validate test trees.
+  parser::BracketNotationParser bnp;
+  if (!bnp.validate_input(ti.t1)) {
+    std::cerr << "Incorrect format of source tree: '" << ti.t1 << "'. Is the number of opening and closing brackets equal?" << std::endl;
+    return -1;
+  }
+  if (!bnp.validate_input(ti.t2)) {
+    std::cerr << "Incorrect format of destination tree: '" << ti.t2 << "'. Is the number of opening and closing brackets equal?" << std::endl;
+    return -1;
+  }
+  // Parse test tree.
+  node::Node<Label> t1 = bnp.parse_single(ti.t1);
+  node::Node<Label> t2 = bnp.parse_single(ti.t2);
+  
+  std::cout << "INPUT T1\n" << ti.t1 << std::endl;
+  std::cout << "INPUT T2\n" << ti.t2 << std::endl;
 
+  // Initialise Touzet algorithm.
+  ted::Touzet<Label, CostModel> touzet_ted;
+  
+  // Initialise Zhang and Shsha's algorithm.
+  ted::ZhangShasha<Label, CostModel> zs_ted;
+  
+  // Runtime measurement.
+  Timing timing;
+  Timing::Interval runtime("runtime");
+    
+  double computed_result = -1;
+  double expected_result = -1;
+  double result_zs = -1;
+  
+  if (tp.set_zs_k || tp.alg_zs_ted) {
+    runtime.reset();
+    runtime.start();
+    result_zs = zs_ted.zhang_shasha_ted(t1, t2);
+    runtime.stop();
+  }
+  
+  if (tp.verify_expected) {
+    expected_result = ti.expected_result;
+  }
+  if (tp.set_zs_k) {
+    ti.k = std::ceil(result_zs);
+    expected_result = result_zs;
+  }
+  
+  std::cout << "K = " << ti.k << std::endl;
+  std::cout << "EXPECTED = " << expected_result << std::endl;
+  
+  if (tp.set_zs_k || tp.alg_zs_ted) {
+    std::cout << "ZS_TED" << std::endl;
+    std::cout << " ted : " << result_zs << std::endl;
+    std::cout << "  sp : " << zs_ted.get_subproblem_count() << std::endl;
+    std::cout << "time : " << runtime.getfloat() << std::endl;
+  }
+  
+  try {
+    if (tp.alg_touzet_ted) {
+      runtime.reset();
+      runtime.start();
+      computed_result = touzet_ted.touzet_ted(t1, t2, ti.k);
+      runtime.stop();
+      std::cout << "TOUZET_TED" << std::endl;
+      std::cout << " ted : " << computed_result << std::endl;
+      std::cout << "  sp : " << touzet_ted.get_subproblem_count() << std::endl;
+      std::cout << "time : " << runtime.getfloat() << std::endl;
+      if (compare_results(computed_result, expected_result, tp) < 0) {
+        return -1;
+      }
+    }
+    if (tp.alg_touzet_ted_depth_pruning) {
+      runtime.reset();
+      runtime.start();
+      computed_result = touzet_ted.touzet_ted_depth_pruning(t1, t2, ti.k);
+      runtime.stop();
+      std::cout << "TOUZET_TED_DEPTH_PRUNING" << std::endl;
+      std::cout << " ted : " << computed_result << std::endl;
+      std::cout << "  sp : " << touzet_ted.get_subproblem_count() << std::endl;
+      std::cout << "time : " << runtime.getfloat() << std::endl;
+      if (compare_results(computed_result, expected_result, tp) < 0) {
+        return -1;
+      }
+    }
+    if (tp.alg_touzet_ted_kr_loop) {
+      runtime.reset();
+      runtime.start();
+      computed_result = touzet_ted.touzet_ted_kr_loop(t1, t2, ti.k);
+      runtime.stop();
+      std::cout << "TOUZET_TED_KR_LOOP" << std::endl;
+      std::cout << " ted : " << computed_result << std::endl;
+      std::cout << "  sp : " << touzet_ted.get_subproblem_count() << std::endl;
+      std::cout << "time : " << runtime.getfloat() << std::endl;
+      if (compare_results(computed_result, expected_result, tp) < 0) {
+        return -1;
+      }
+    }
+    if (tp.alg_touzet_ted_kr_set) {
+      runtime.reset();
+      runtime.start();
+      computed_result = touzet_ted.touzet_ted_kr_set(t1, t2, ti.k);
+      runtime.stop();
+      std::cout << "TOUZET_TED_KR_SET" << std::endl;
+      std::cout << " ted : " << computed_result << std::endl;
+      std::cout << "  sp : " << touzet_ted.get_subproblem_count() << std::endl;
+      std::cout << "time : " << runtime.getfloat() << std::endl;
+      if (compare_results(computed_result, expected_result, tp) < 0) {
+        return -1;
+      }
+    }
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+  
+  return 0;
+};
+
+
+
+int main(int argc, char** argv) {
+  // Command line interface.
+  //
+  // - File with test cases.
+  // - Choose algorithm's version (or more) to execute.
+  // - Either verify expected value or compare the result to ZS.
+  //   - If set k to ZS, k=ZS.
+  //   - If verify, k and expected are given.
+  // - Include random.
+  
+  TestParams tp;
+  
+  if (argc > 0) {
+    std::vector<std::string> arguments(argv, argv + argc);
+    for (auto a : arguments) {
+      std::string string_a(a);
+      if (string_a.find("--input=") != std::string::npos) {
+        tp.input_file = string_a.substr(string_a.find('=') + 1, string_a.size());
+      }
+      if (string_a == "--verify") {
+        tp.verify_expected = true;
+      }
+      if (string_a == "--set-zs-k") {
+        tp.set_zs_k = true;
+      }
+      if (string_a == "-t") {
+        tp.alg_touzet_ted = true;
+      }
+      if (string_a == "-td") {
+        tp.alg_touzet_ted_depth_pruning = true;
+      }
+      if (string_a == "-tl") {
+        tp.alg_touzet_ted_kr_loop = true;
+      }
+      if (string_a == "-ts") {
+        tp.alg_touzet_ted_kr_set = true;
+      }
+      if (string_a == "-zs") {
+        tp.alg_zs_ted = true;
+      }
+      if (string_a == "--include-random") {
+        tp.include_random = true;
+      }
+      if (string_a == "--lower-than-threshold") {
+        tp.lower_than_threshold = true;
+      }
+    }
+  } else {
+    tp.set_default();
+  }
+  
+  std::cout << "************************************* TEST PARAMS" << std::endl;
+  std::cout << "input_file = " << tp.input_file << std::endl;
+  std::cout << "verify_expected = " << tp.verify_expected << std::endl;
+  std::cout << "set_zs_k = " << tp.set_zs_k << std::endl;
+  std::cout << "lower_than_threshold = " << tp.lower_than_threshold << std::endl;
+  std::cout << "alg_touzet_ted = " << tp.alg_touzet_ted << std::endl;
+  std::cout << "alg_touzet_ted_depth_pruning = " << tp.alg_touzet_ted_depth_pruning << std::endl;
+  std::cout << "alg_touzet_ted_kr_loop = " << tp.alg_touzet_ted_kr_loop << std::endl;
+  std::cout << "alg_touzet_ted_kr_set = " << tp.alg_touzet_ted_kr_set << std::endl;
+  std::cout << "alg_zs_ted = " << tp.alg_zs_ted << std::endl;
+  
   // Parse test cases from file.
-  std::ifstream test_cases_file("touzet_ted_test_data.txt");
+  std::ifstream test_cases_file(tp.input_file);
   if (!test_cases_file.is_open()) {
     std::cerr << "Error while opening file." << std::endl;
     return -1;
   }
 
-  parser::BracketNotationParser bnp;
-
-  // Initialise Touzet algorithm.
-  ted::Touzet<Label, CostModel> touzet_ted;
-
-  // Initialise Zhang and Shsha's algorithm.
-  ted::ZhangShasha<Label, CostModel> zs_ted;
-
-
   // Fixed test cases.
-
-  std::cout << "--- FIXED TEST ---" << std::endl;
+  std::cout << "************************************** FIXED TEST" << std::endl;
 
   // Read test cases from a file line by line.
-  for (std::string line; std::getline( test_cases_file, line);) {
-    if (line[0] == '#') {
-
-      std::cout << "--- TEST CASE ---" << std::endl;
-      std::cout << line << std::endl;
-
-      // Read the single test case.
+  for (std::string line; std::getline(test_cases_file, line);) {
+    TestInput ti;
+    // Enable multiline comments.
+    while (line[0] == '#') {
+      ti.description += line;
       std::getline(test_cases_file, line);
-      std::string input_tree_1_string = line;
-      std::getline(test_cases_file, line);
-      std::string input_tree_2_string = line;
-      std::getline(test_cases_file, line);
-      int k = std::stoi(line);
-      std::getline(test_cases_file, line);
-      double correct_result = std::stod(line);
-
-      // parser::BracketNotationParser bnp;
-
-      // Validate test trees.
-      if (!bnp.validate_input(input_tree_1_string)) {
-        std::cerr << "Incorrect format of source tree: '" << input_tree_1_string << "'. Is the number of opening and closing brackets equal?" << std::endl;
-        return -1;
-      }
-      if (!bnp.validate_input(input_tree_2_string)) {
-        std::cerr << "Incorrect format of destination tree: '" << input_tree_2_string << "'. Is the number of opening and closing brackets equal?" << std::endl;
-        return -1;
-      }
-      // Parse test tree.
-      node::Node<Label> t1 = bnp.parse_single(input_tree_1_string);
-      node::Node<Label> t2 = bnp.parse_single(input_tree_2_string);
-
-
-      // Execute the algorithm WITHOUT redundancy.
-      double computed_results;
-      try {
-        computed_results = touzet_ted.touzet_ted_kr_set(t1, t2, k);
-      } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-      }
-
-      if (correct_result != computed_results) {
-        std::cerr << "Incorrect TED result (WITHOUT redundancy): " << computed_results << " instead of " << correct_result << std::endl;
-        std::cerr << "T1: " << input_tree_1_string << std::endl;
-        std::cerr << "T2: " << input_tree_2_string << std::endl;
-        std::cerr << "k: " << k << std::endl;
-        return -1;
-      }
     }
-  }
-
-  // Randomised test cases.
-
-  std::cout << "--- RANDOMIZED TEST ---" << std::endl;
-  
-  // Runtime measurement.
-  Timing timing;
-  
-  // Initialise SimpleTreeGenerator and generate a dummy tree.
-  tree_generator::SimpleTreeGenerator stg;
-  int max_nodes = 100;
-  int max_edits = 30; // 30%
-  std::mt19937 rd;
-  std::uniform_int_distribution<int> edits_dist(0, max_edits);
-  std::string t1_gen_string;
-  std::string t2_gen_string;
-  double touzet_result;
-  double touzet_result_d;
-  double touzet_result_kr_loop;
-  double touzet_result_kr_set;
-  double zs_result;
-  int k;
-  for (int i = 0; i < 200; ++i) {
-    t1_gen_string = stg.generate_tree(max_nodes);
-    t2_gen_string = stg.modify_tree(t1_gen_string, max_nodes, edits_dist(rd));
-    std::cout << t1_gen_string << std::endl;
-    std::cout << t2_gen_string << std::endl;
-    node::Node<Label> t1_gen = bnp.parse_single(t1_gen_string);
-    node::Node<Label> t2_gen = bnp.parse_single(t2_gen_string);
-    try {
-      Timing::Interval * zs_time = timing.create_enroll("zs_time");
-      zs_time->start();
-      zs_result = zs_ted.zhang_shasha_ted(t1_gen, t2_gen);
-      zs_time->stop();
-      k = int(std::ceil(zs_result));
-      std::cout << "                      k = " << k << std::endl;
-      std::cout << "---------------------------------------------" << std::endl;
-      std::cout << "              zs_result = " << zs_result << std::endl;
-      std::cout << "               zs subpr = " << zs_ted.get_subproblem_count() << std::endl;
-      std::cout << "             zs runtime = " << zs_time->getfloat() << std::endl;
-      std::cout << "---------------------------------------------" << std::endl;
-      Timing::Interval * touzet_time = timing.create_enroll("touzet_time");
-      touzet_time->start();
-      touzet_result = touzet_ted.touzet_ted(t1_gen, t2_gen, k);
-      touzet_time->stop();
-      std::cout << "          touzet_result = " << touzet_result << std::endl;
-      std::cout << "           touzet subpr = " << touzet_ted.get_subproblem_count() << std::endl;
-      std::cout << "         touzet runtime = " << touzet_time->getfloat() << std::endl;
-      std::cout << "---------------------------------------------" << std::endl;
-      Timing::Interval * touzet_d_time = timing.create_enroll("touzet_d_time");
-      touzet_d_time->start();
-      touzet_result_d = touzet_ted.touzet_ted_depth_pruning(t1_gen, t2_gen, k);
-      touzet_d_time->stop();
-      std::cout << "        touzet_result_d = " << touzet_result_d << std::endl;
-      std::cout << "         touzet d subpr = " << touzet_ted.get_subproblem_count() << std::endl;
-      std::cout << "       touzet d runtime = " << touzet_d_time->getfloat() << std::endl;
-      std::cout << "---------------------------------------------" << std::endl;
-      Timing::Interval * touzet_kr_loop_time = timing.create_enroll("touzet_kr_loop_time");
-      touzet_kr_loop_time->start();
-      touzet_result_kr_loop = touzet_ted.touzet_ted_kr_loop(t1_gen, t2_gen, k);
-      touzet_kr_loop_time->stop();
-      std::cout << "  touzet_result_kr_loop = " << touzet_result_kr_loop << std::endl;
-      std::cout << "   touzet kr loop subpr = " << touzet_ted.get_subproblem_count() << std::endl;
-      std::cout << " touzet kr loop runtime = " << touzet_kr_loop_time->getfloat() << std::endl;
-      std::cout << "---------------------------------------------" << std::endl;
-      Timing::Interval * touzet_kr_set_time = timing.create_enroll("touzet_kr_set_time");
-      touzet_kr_set_time->start();
-      touzet_result_kr_set = touzet_ted.touzet_ted_kr_set(t1_gen, t2_gen, k);
-      touzet_kr_set_time->stop();
-      std::cout << "   touzet_result_kr_set = " << touzet_result_kr_set << std::endl;
-      std::cout << "    touzet kr set subpr = " << touzet_ted.get_subproblem_count() << std::endl;
-      std::cout << "  touzet kr set runtime = " << touzet_kr_set_time->getfloat() << std::endl;
-      std::cout << "---------------------------------------------" << std::endl;
-    } catch (const std::exception& e) {
-      std::cerr << e.what() << std::endl;
+    // Read the single test case.
+    ti.t1 = line;
+    std::getline(test_cases_file, line);
+    ti.t2 = line;
+    if (tp.verify_expected) {
+      std::getline(test_cases_file, line);
+      ti.k = std::stoi(line);
+      std::getline(test_cases_file, line);
+      ti.expected_result = std::stod(line);
     }
-
-    if (zs_result != touzet_result ||
-        zs_result != touzet_result_d ||
-        zs_result != touzet_result_kr_loop ||
-        zs_result != touzet_result_kr_set
-      ) {
-      std::cerr << "Incorrect TED result (Touzet | Touzet_D | Touzet_KR_LOOP | Touzet_KR_SET vs ZS): " <<
-          touzet_result << " | " <<
-          touzet_result_d << " | " <<
-          touzet_result_kr_loop << " | " <<
-          touzet_result_kr_set <<
-          " instead of " << zs_result << std::endl;
-      std::cerr << "T1: " << t1_gen_string << std::endl;
-      std::cerr << "T2: " << t2_gen_string << std::endl;
-      std::cerr << "k: " << k << std::endl;
+    if (single_test_case(tp, ti) < 0) {
       return -1;
     }
   }
 
-
+  if (tp.include_random) {
+    // Randomised test cases.
+    std::cout << "********************************* RANDOMIZED TEST" << std::endl;
+    // We have to compare to ZS because the k is unknown.
+    tp.set_zs_k = true;
+    tp.lower_than_threshold = false;
+    tp.verify_expected = false;
+    // Initialise SimpleTreeGenerator and generate a dummy tree.
+    tree_generator::SimpleTreeGenerator stg;
+    int max_nodes = 100;
+    int max_edits = 30;
+    std::mt19937 rd;
+    std::uniform_int_distribution<int> edits_dist(0, max_edits);
+    // Do 200 test cases.
+    for (int i = 0; i < 200; ++i) {
+      TestInput ti;
+      ti.t1 = stg.generate_tree(max_nodes);
+      ti.t2 = stg.modify_tree(ti.t1, max_nodes, edits_dist(rd));
+      if (single_test_case(tp, ti) < 0) {
+        return -1;
+      }
+    }
+  }
 
   return 0;
 }
