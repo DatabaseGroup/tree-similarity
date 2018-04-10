8916589cec906486011ea918b40e7d72487474a3
Mateusz Pawlik <mpawlik@.cosy.sbg.ac.at>
2018-04-10 13:55:05 +0200

    greedy_ub: Fixed bugs in revising one-to-one mappings.

--- src/ted_ub/greedy_ub_impl.h
+++ src/ted_ub/greedy_ub_impl.h
@@ -103,21 +103,54 @@ std::vector<std::pair<int, int>> GreedyUB<Label, CostModel>::to_ted_mapping(std:
   int cur_t2 = 0;
   int prev_t1 = -1;
   int prev_t2 = -1;
+  bool mapped_t1_node_processed  = false;
+  bool mapped_t2_node_processed  = false;
   for (auto m : mapping) {
     cur_t1 = m.first;
     cur_t2 = m.second;
     std::cout << "M(" << cur_t1 << "," << cur_t2 << ")" << std::endl;
     
     // Increasing postorder id test.
-    if (cur_t1 < prev_t1 || cur_t2 < prev_t2) {
+    // The pirs in the input one-to-one mapping are sorted on the first element.
+    // The second elements of two consecutive pairs must not have descreasing
+    // values.
+    if (cur_t2 < prev_t2) {
       std::cout << "M DEL post(" << cur_t1 << "," << cur_t2 << ")" << std::endl;
       continue;
     }
     
+    // Go back with t1_i and t2_i if the current node ids are smaller.
+    // If cur_t1 < t1_i => cur_t1 has been processed as a non-mapped node and
+    // it has to be processed as a mapped node, rolling back the changes made
+    // to it
+    // If cur_t1 == t1_i => cur_t1 has to be processed as a mapped node,
+    // it has not been processed before.
+    if (cur_t1 < t1_i) {
+      t1_i = cur_t1;
+      mapped_t1_node_processed = true;
+      std::cout << "t1_i = " << t1_i << " processed before as non-mapped" << std::endl;
+    }
+    if (cur_t2 < t2_i) {
+      t2_i = cur_t2;
+      mapped_t2_node_processed = true;
+      std::cout << "t2_i = " << t2_i << " processed before as non-mapped" << std::endl;
+    }
+    if (cur_t1 == t1_i) {
+      mapped_t1_node_processed = false;
+    }
+    if (cur_t2 == t2_i) {
+      mapped_t2_node_processed = false;
+    }
+    
     // TODO: Review the following if statements.
     
-    // T1
-    while (t1_i < cur_t1) { // nodes not in mapping
+    // NOTE: is_leaf : t1_node_[i].get().is_leaf() [with t1_size_[i] == 1 may be faster]
+    // NOTE: first_leaf_to_the_right : precompute
+    // NOTE: is_leftmost_child_of_its_parent : precompute OR post_to_pre[parent[i]] + 1 == post_to_pre[i]
+    
+    // Nodes not in mapping in T1.
+    while (t1_i < cur_t1) {
+      std::cout << "processing t1_i = " << t1_i << std::endl;
       // descendants
       t1_count_mapped_desc[t1_parent_[t1_i]] += t1_count_mapped_desc[t1_i];
       // left
@@ -133,31 +166,8 @@ std::vector<std::pair<int, int>> GreedyUB<Label, CostModel>::to_ted_mapping(std:
       }
       ++t1_i;
     }
-    // mapped node
-    if (t1_parent_[t1_i] > -1) { // root has no parent
-      // descendants
-      t1_count_mapped_desc[t1_parent_[t1_i]] += t1_count_mapped_desc[t1_i] + 1;
-      // left
-      if (t1_rch_[t1_i] > -1) {
-        if (t1_node_[t1_i].get().is_leaf()) { // leaf node
-          t1_count_mapped_left[t1_rch_[t1_i]] = t1_count_mapped_left[t1_i] + 1;
-        } else { // inner node
-          t1_count_mapped_left[t1_rch_[t1_i]] += 1;
-        }
-      }
-      if (t1_post_to_pre_[t1_parent_[t1_i]] + 1 == t1_post_to_pre_[t1_i]) { // leftmost child of its parent
-                                                                            // NOTE: Could be precomputed.
-        t1_count_mapped_left[t1_parent_[t1_i]] = t1_count_mapped_left[t1_i];
-      }
-    }
-    ++t1_i;
-    
-    // NOTE: is_leaf : t1_node_[i].get().is_leaf() [with t1_size_[i] == 1 may be faster]
-    // NOTE: first_leaf_to_the_right : precompute
-    // NOTE: is_leftmost_child_of_its_parent : precompute OR post_to_pre[parent[i]] + 1 == post_to_pre[i]
-    
-    // T2
-    while (t2_i < cur_t2) { // nodes not in mapping
+    // Nodes not in mapping in T2.
+    while (t2_i < cur_t2) {
       std::cout << "processing t2_i = " << t2_i << std::endl;
       // descendants
       t2_count_mapped_desc[t2_parent_[t2_i]] += t2_count_mapped_desc[t2_i];
@@ -173,35 +183,78 @@ std::vector<std::pair<int, int>> GreedyUB<Label, CostModel>::to_ted_mapping(std:
       }
       ++t2_i;
     }
-    // mapped node
-    std::cout << "processing t2_i = " << t2_i << std::endl;
-    if (t2_parent_[t2_i] > -1) { // root has no parent
-      // descendants
-      t2_count_mapped_desc[t2_parent_[t2_i]] += t2_count_mapped_desc[t2_i] + 1;
-      // left
-      if (t2_rch_[t2_i] > -1) {
-        if (t2_node_[t2_i].get().is_leaf()) { // leaf node
-          t2_count_mapped_left[t2_rch_[t2_i]] = t2_count_mapped_left[t2_i] + 1;
-        } else { // inner node
-          t2_count_mapped_left[t2_rch_[t2_i]] += 1;
-        }
-      }
-      if (t2_post_to_pre_[t2_parent_[t2_i]] + 1 == t2_post_to_pre_[t2_i]) { // leftmost child of its parent
-        t2_count_mapped_left[t2_parent_[t2_i]] = t2_count_mapped_left[t2_i];
-      }
-    }
-    ++t2_i;
+    
+    // If the current node pair cannot be mapped, the nodes must not be
+    // processed as mapped (the next two if statements).
     
     // Mapped descendants test.
     if (t1_count_mapped_desc[cur_t1] != t2_count_mapped_desc[cur_t2]) {
       std::cout << "M DEL desc(" << cur_t1 << "," << cur_t2 << ")" << std::endl;
       continue;
     }
-    
     // Mapped nodes to the left test.
     if (t1_count_mapped_left[cur_t1] != t2_count_mapped_left[cur_t2]) {
       std::cout << "M DEL left(" << cur_t1 << "," << cur_t2 << ")" << std::endl;
       continue;
+    }
+    
+    // Mapped node in T1.
+    if (!mapped_t1_node_processed) {
+      std::cout << "processing t1_i = " << t1_i << std::endl;
+      if (t1_parent_[t1_i] > -1) { // root has no parent
+        // descendants
+        t1_count_mapped_desc[t1_parent_[t1_i]] += t1_count_mapped_desc[t1_i] + 1;
+        // left
+        if (t1_rch_[t1_i] > -1) {
+          if (t1_node_[t1_i].get().is_leaf()) { // leaf node
+            t1_count_mapped_left[t1_rch_[t1_i]] = t1_count_mapped_left[t1_i] + 1;
+          } else { // inner node
+            t1_count_mapped_left[t1_rch_[t1_i]] += 1;
+          }
+        }
+        if (t1_post_to_pre_[t1_parent_[t1_i]] + 1 == t1_post_to_pre_[t1_i]) { // leftmost child of its parent
+                                                                              // NOTE: Could be precomputed.
+          t1_count_mapped_left[t1_parent_[t1_i]] = t1_count_mapped_left[t1_i];
+        }
+      }
+      ++t1_i;
+    } else { // The current mapped node has been processed and it's enough to
+             // fix the values it updated.
+      std::cout << "processing t1_i = " << t1_i << std::endl;
+      if (t1_parent_[t1_i] > -1) { // root has no parent
+        // descendants
+        t1_count_mapped_desc[t1_parent_[t1_i]] += 1; // THE ONLY DIFF TO ABOVE.
+      }
+      ++t1_i;
+    }
+    // Mapped node in T2.
+    if (!mapped_t2_node_processed) {
+      // mapped node
+      std::cout << "processing t2_i = " << t2_i << std::endl;
+      if (t2_parent_[t2_i] > -1) { // root has no parent
+        // descendants
+        t2_count_mapped_desc[t2_parent_[t2_i]] += t2_count_mapped_desc[t2_i] + 1;
+        // left
+        if (t2_rch_[t2_i] > -1) {
+          if (t2_node_[t2_i].get().is_leaf()) { // leaf node
+            t2_count_mapped_left[t2_rch_[t2_i]] = t2_count_mapped_left[t2_i] + 1;
+          } else { // inner node
+            t2_count_mapped_left[t2_rch_[t2_i]] += 1;
+          }
+        }
+        if (t2_post_to_pre_[t2_parent_[t2_i]] + 1 == t2_post_to_pre_[t2_i]) { // leftmost child of its parent
+          t2_count_mapped_left[t2_parent_[t2_i]] = t2_count_mapped_left[t2_i];
+        }
+      }
+      ++t2_i;
+    } else { // The current mapped node has been processed and it's enough to
+             // fix the values it updated.
+      std::cout << "processing t2_i = " << t2_i << std::endl;
+      if (t2_parent_[t2_i] > -1) { // root has no parent
+        // descendants
+        t2_count_mapped_desc[t2_parent_[t2_i]] += 1; // THE ONLY DIFF TO ABOVE.
+      }
+      ++t2_i;
     }
     
     ted_mapping.push_back({cur_t1, cur_t2});
@@ -209,6 +262,7 @@ std::vector<std::pair<int, int>> GreedyUB<Label, CostModel>::to_ted_mapping(std:
     prev_t1 = cur_t1;
     prev_t2 = cur_t2;
   }
+  
   // TODO: Gap after the last mapped node until the last node in the tree. 
   
   std::cout << "t1_count_mapped_desc : ";
