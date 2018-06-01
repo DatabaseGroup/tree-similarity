221fced776cea141f3d436a134a8b86b8897badd
Mateusz Pawlik <mpawlik@.cosy.sbg.ac.at>
2018-06-01 10:46:31 +0200

    greedy_ub: ted_test modified to test multiple algorithms.

--- test/greedy_ub/greedy_ub_ted_test.cc
+++ test/greedy_ub/greedy_ub_ted_test.cc
@@ -17,31 +17,42 @@
 
 struct TestParams {
   std::string input_file;
-  bool verify_expected;
   bool set_zs_k;
+  bool fixed_k;
+  int k;
+  bool alg_touzet_ted;
+  bool alg_touzet_ted_depth_pruning;
+  bool alg_touzet_ted_kr_loop;
   bool alg_touzet_ted_kr_set;
   bool alg_zs_ted;
   bool alg_greedy_ub;
-  bool lower_than_threshold;
+  bool alg_greedy_ub_deprecated;
   
   TestParams() {
     input_file = "";
-    verify_expected = false;
     set_zs_k = false;
+    fixed_k = false;
+    k = 0;
+    alg_touzet_ted = false;
+    alg_touzet_ted_depth_pruning = false;
+    alg_touzet_ted_kr_loop = false;
     alg_touzet_ted_kr_set = false;
     alg_zs_ted = false;
     alg_greedy_ub = false;
-    lower_than_threshold = false;
+    alg_greedy_ub_deprecated = false;
   };
   
   void set_default() {
     input_file = "bozen_result_pairs_k7.txt";
-    verify_expected = false;
     set_zs_k = true;
-    alg_touzet_ted_kr_set = false;
+    fixed_k = false;
+    alg_touzet_ted = true;
+    alg_touzet_ted_depth_pruning = true;
+    alg_touzet_ted_kr_loop = true;
+    alg_touzet_ted_kr_set = true;
     alg_zs_ted = true;
     alg_greedy_ub = true;
-    lower_than_threshold = false;
+    alg_greedy_ub_deprecated = true;
   };
 };
 
@@ -58,15 +69,16 @@ void communicate_result_error(double computed, double expected) {
 };
 
 int compare_results(double computed, double expected, TestParams& tp) {
-  if (tp.lower_than_threshold) {
-    if (computed > expected) {
-      communicate_result_error(computed, expected);
-      return -1;
-    }
-  } else if (computed != expected) {
-    communicate_result_error(computed, expected);
-    return -1;
-  }
+  // if (tp.lower_than_threshold) {
+  //   if (computed > expected) {
+  //     communicate_result_error(computed, expected);
+  //     return -1;
+  //   }
+  // } else 
+  // if (computed  expected) {
+  //   communicate_result_error(computed, expected);
+  //   return -1;
+  // }
   return 0;
 }
 
@@ -119,12 +131,17 @@ int single_test_case(TestParams& tp, TestInput& ti) {
     runtime.stop();
   }
   
-  if (tp.verify_expected) {
-    expected_result = ti.expected_result;
-  }
+  // if (tp.verify_expected) {
+  //   expected_result = ti.expected_result;
+  // }
+  
+  expected_result = result_zs;
+  
   if (tp.set_zs_k) {
     ti.k = std::ceil(result_zs);
-    expected_result = result_zs;
+  }
+  if (tp.fixed_k) {
+    ti.k = tp.k;
   }
   
   std::cout << "K = " << ti.k << std::endl;
@@ -138,6 +155,45 @@ int single_test_case(TestParams& tp, TestInput& ti) {
   }
   
   try {
+    if (tp.alg_touzet_ted) {
+      runtime.reset();
+      runtime.start();
+      computed_result = touzet_ted.touzet_ted(t1, t2, ti.k);
+      runtime.stop();
+      std::cout << "TOUZET_TED" << std::endl;
+      std::cout << " ted : " << computed_result << std::endl;
+      std::cout << "  sp : " << touzet_ted.get_subproblem_count() << std::endl;
+      std::cout << "time : " << runtime.getfloat() << std::endl;
+      if (compare_results(computed_result, expected_result, tp) < 0) {
+        return -1;
+      }
+    }
+    if (tp.alg_touzet_ted_depth_pruning) {
+      runtime.reset();
+      runtime.start();
+      computed_result = touzet_ted.touzet_ted_depth_pruning(t1, t2, ti.k);
+      runtime.stop();
+      std::cout << "TOUZET_TED_DEPTH_PRUNING" << std::endl;
+      std::cout << " ted : " << computed_result << std::endl;
+      std::cout << "  sp : " << touzet_ted.get_subproblem_count() << std::endl;
+      std::cout << "time : " << runtime.getfloat() << std::endl;
+      if (compare_results(computed_result, expected_result, tp) < 0) {
+        return -1;
+      }
+    }
+    if (tp.alg_touzet_ted_kr_loop) {
+      runtime.reset();
+      runtime.start();
+      computed_result = touzet_ted.touzet_ted_kr_loop(t1, t2, ti.k);
+      runtime.stop();
+      std::cout << "TOUZET_TED_KR_LOOP" << std::endl;
+      std::cout << " ted : " << computed_result << std::endl;
+      std::cout << "  sp : " << touzet_ted.get_subproblem_count() << std::endl;
+      std::cout << "time : " << runtime.getfloat() << std::endl;
+      if (compare_results(computed_result, expected_result, tp) < 0) {
+        return -1;
+      }
+    }
     if (tp.alg_touzet_ted_kr_set) {
       runtime.reset();
       runtime.start();
@@ -155,45 +211,38 @@ int single_test_case(TestParams& tp, TestInput& ti) {
       runtime.reset();
       runtime.start();
       // std::vector<std::pair<int, int>> m = greedy_ub.lb_mapping(t1, t2, 7);
+      std::vector<std::pair<int, int>> m = greedy_ub.lb_mapping_fill_gaps(t1, t2, ti.k);
+      computed_result = greedy_ub.mapping_cost(m);//touzet_ted.touzet_ted_kr_set(t1, t2, ti.k);
+      runtime.stop();
+      std::cout << "GREEDY_UB" << std::endl;
+      // std::cout << "   m : ";
+      // for (auto e : m) {
+      //   std::cout << "(" << e.first << "," << e.second << ") ";
+      // }
+      // std::cout << std::endl;
+      std::cout << " ted : " << computed_result << std::endl;
+      std::cout << "time : " << runtime.getfloat() << std::endl;
+      if (compare_results(computed_result, expected_result, tp) < 0) {
+        return -1;
+      }
+    }
+    if (tp.alg_greedy_ub_deprecated) {
+      runtime.reset();
+      runtime.start();
+      // std::vector<std::pair<int, int>> m = greedy_ub.lb_mapping(t1, t2, 7);
       std::vector<std::pair<int, int>> m = greedy_ub.lb_mapping_fill_gaps_deprecated(t1, t2, ti.k);
       computed_result = greedy_ub.mapping_cost(m);//touzet_ted.touzet_ted_kr_set(t1, t2, ti.k);
       runtime.stop();
       std::cout << "GREEDY_UB_DEPRECATED" << std::endl;
-      std::cout << "   m : ";
-      for (auto e : m) {
-        std::cout << "(" << e.first << "," << e.second << ") ";
-      }
-      std::cout << std::endl;
-      std::cout << " ted : " << computed_result << std::endl;
-      std::cout << "time : " << runtime.getfloat() << std::endl;
-      if (computed_result < expected_result) {
-        communicate_result_error(computed_result, expected_result);
-        return -1;
-      }
-      // if (computed_result > expected_result) {
-      //   return 1;
+      // std::cout << "   m : ";
+      // for (auto e : m) {
+      //   std::cout << "(" << e.first << "," << e.second << ") ";
       // }
-      
-      runtime.reset();
-      runtime.start();
-      // std::vector<std::pair<int, int>> m = greedy_ub.lb_mapping(t1, t2, 7);
-      m = greedy_ub.lb_mapping_fill_gaps(t1, t2, ti.k);
-      computed_result = greedy_ub.mapping_cost(m);//touzet_ted.touzet_ted_kr_set(t1, t2, ti.k);
-      runtime.stop();
-      std::cout << "GREEDY_UB" << std::endl;
-      std::cout << "   m : ";
-      for (auto e : m) {
-        std::cout << "(" << e.first << "," << e.second << ") ";
-      }
-      std::cout << std::endl;
+      // std::cout << std::endl;
       std::cout << " ted : " << computed_result << std::endl;
       std::cout << "time : " << runtime.getfloat() << std::endl;
-      if (computed_result < expected_result) {
-        communicate_result_error(computed_result, expected_result);
+      if (compare_results(computed_result, expected_result, tp) < 0) {
         return -1;
-      }
-      if (computed_result > expected_result) {
-        return 1;
       }
     }
   } catch (const std::exception& e) {
@@ -224,11 +273,23 @@ int main(int argc, char** argv) {
       if (string_a.find("--input=") != std::string::npos) {
         tp.input_file = string_a.substr(string_a.find('=') + 1, string_a.size());
       }
-      if (string_a == "--verify") {
-        tp.verify_expected = true;
-      }
       if (string_a == "--set-zs-k") {
         tp.set_zs_k = true;
+        tp.fixed_k = false;
+      }
+      if (string_a.find("--fixed-k=") != std::string::npos) {
+        tp.k = std::stoi(string_a.substr(string_a.find('=') + 1, string_a.size()));
+        tp.fixed_k = true;
+        tp.set_zs_k = false;
+      }
+      if (string_a == "-t") {
+        tp.alg_touzet_ted = true;
+      }
+      if (string_a == "-td") {
+        tp.alg_touzet_ted_depth_pruning = true;
+      }
+      if (string_a == "-tl") {
+        tp.alg_touzet_ted_kr_loop = true;
       }
       if (string_a == "-ts") {
         tp.alg_touzet_ted_kr_set = true;
@@ -236,11 +297,11 @@ int main(int argc, char** argv) {
       if (string_a == "-g") {
         tp.alg_greedy_ub = true;
       }
+      if (string_a == "-gd") {
+        tp.alg_greedy_ub_deprecated = true;
+      }
       if (string_a == "-zs") {
         tp.alg_zs_ted = true;
-      }
-      if (string_a == "--lower-than-threshold") {
-        tp.lower_than_threshold = true;
       }
     }
     if (tp.input_file == "") {
@@ -253,12 +314,16 @@ int main(int argc, char** argv) {
   
   std::cout << "************************************* TEST PARAMS" << std::endl;
   std::cout << "input_file = " << tp.input_file << std::endl;
-  std::cout << "verify_expected = " << tp.verify_expected << std::endl;
   std::cout << "set_zs_k = " << tp.set_zs_k << std::endl;
-  std::cout << "lower_than_threshold = " << tp.lower_than_threshold << std::endl;
+  std::cout << "fixed_k = " << tp.fixed_k << std::endl;
+  std::cout << "k = " << tp.k << std::endl;
+  std::cout << "alg_touzet_ted = " << tp.alg_touzet_ted << std::endl;
+  std::cout << "alg_touzet_ted_depth_pruning = " << tp.alg_touzet_ted_depth_pruning << std::endl;
+  std::cout << "alg_touzet_ted_kr_loop = " << tp.alg_touzet_ted_kr_loop << std::endl;
   std::cout << "alg_touzet_ted_kr_set = " << tp.alg_touzet_ted_kr_set << std::endl;
   std::cout << "alg_zs_ted = " << tp.alg_zs_ted << std::endl;
   std::cout << "alg_greedy_ub = " << tp.alg_greedy_ub << std::endl;
+  std::cout << "alg_greedy_ub_deprecated = " << tp.alg_greedy_ub_deprecated << std::endl;
   
   std::ifstream test_cases_file;
   if (tp.input_file != "") {
@@ -273,8 +338,6 @@ int main(int argc, char** argv) {
   // Fixed test cases.
   std::cout << "************************************** FIXED TEST" << std::endl;
 
-  unsigned int ub_g_k = 0;
-
   // Read test cases from a file line by line.
   for (std::string line; std::getline(test_cases_file, line);) {
     TestInput ti;
@@ -287,22 +350,11 @@ int main(int argc, char** argv) {
     ti.t1 = line;
     std::getline(test_cases_file, line);
     ti.t2 = line;
-    if (tp.verify_expected) {
-      std::getline(test_cases_file, line);
-      ti.k = std::stoi(line);
-      std::getline(test_cases_file, line);
-      ti.expected_result = std::stod(line);
-    }
     int stc_res = single_test_case(tp, ti);
     if (stc_res < 0) {
       return -1;
     }
-    if (stc_res == 1) {
-      ++ub_g_k;
-    }
   }
-  
-  std::cout << "----------------- ub_g_k = " << ub_g_k << std::endl;
 
   return 0;
 }
