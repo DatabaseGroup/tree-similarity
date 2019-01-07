5f160f2cd6fbf6457b2064c25eba267641742f9d
Mateusz Pawlik <mpawlik@.cosy.sbg.ac.at>
2019-01-07 15:38:10 +0100

    Implemented String Edit Distance.
    Code copied from Nikolaus's approxlib.
    A few correctness test cases.

---
+++ test/sed_lb/sed_lb_ed_test.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "unit_cost_model.h"
+#include "string_label.h"
+#include "node.h"
+#include "bracket_notation_parser.h"
+#include "sed_lb.h"
+
+int main() {
+
+  using Label = label::StringLabel;
+  using CostModel = cost_model::UnitCostModel<Label>;
+
+  // Parse test cases from file.
+  std::ifstream test_cases_file("sed_lb_ed_test_data.txt");
+  if (!test_cases_file.is_open()) {
+    std::cerr << "Error while opening file." << std::endl;
+    return -1;
+  }
+
+  // Initialise String Edit Distance algorithm.
+  ted_lb::StringEditDistanceLB<Label, CostModel> sed_lb;
+
+  // Read test cases from a file line by line.
+  for (std::string line; std::getline( test_cases_file, line);) {
+    if (line[0] == '#') {
+      // Read the single test case.
+      std::getline(test_cases_file, line);
+      std::string input_tree_1_string = line;
+      std::getline(test_cases_file, line);
+      std::string input_tree_2_string = line;
+      std::getline(test_cases_file, line);
+      double correct_result = std::stod(line);
+
+      parser::BracketNotationParser bnp;
+
+      // Validate test trees.
+      if (!bnp.validate_input(input_tree_1_string)) {
+        std::cerr << "Incorrect format of source tree: '" << input_tree_1_string << "'. Is the number of opening and closing brackets equal?" << std::endl;
+        return -1;
+      }
+      if (!bnp.validate_input(input_tree_2_string)) {
+        std::cerr << "Incorrect format of destination tree: '" << input_tree_2_string << "'. Is the number of opening and closing brackets equal?" << std::endl;
+        return -1;
+      }
+      // Parse test tree.
+      node::Node<Label> t1 = bnp.parse_single(input_tree_1_string);
+      node::Node<Label> t2 = bnp.parse_single(input_tree_2_string);
+
+      // Execute the algorithm.
+      double computed_results = sed_lb.sed_lb_ted(t1, t2);
+
+      if (correct_result != computed_results) {
+        std::cerr << "Incorrect SED result: " << computed_results << " instead of " << correct_result << std::endl;
+        std::cerr << input_tree_1_string << std::endl;
+        std::cerr << input_tree_2_string << std::endl;
+        return -1;
+      }
+    }
+  }
+
+  return 0;
+}
