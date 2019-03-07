19ae856a738798a56a54e7de8b87431d9b15f14e
Mateusz Pawlik <mpawlik@.cosy.sbg.ac.at>
2019-03-07 13:51:13 +0100

    Modified Guha's testing to be similar as for other joins.

--- test/join/guha_test.cc
+++ test/join/guha_test.cc
@@ -13,99 +13,38 @@
 #include "unit_cost_model.h"
 #include "zhang_shasha.h"
 
-/// Convert vector of ResultElement to its string representation.
-/// Sorts the vector in case the result elements don't come in the input order.
-///
-/// TODO: Move this method to some util.
-///
-/// \param v Vector of ResultElement.
-/// \return String representation of v.
-std::string vector_of_re_to_string(std::vector<join::JoinResultElement>& v) {
-  // Sort the vector using lambda function implemented here.
-  std::sort(v.begin(), v.end(), [](const join::JoinResultElement& a,
-                                   const join::JoinResultElement& b){
-    if (a.tree_id_1 < b.tree_id_1) {
-      return true;
-    } else if (a.tree_id_1 == b.tree_id_1) {
-      if (a.tree_id_2 < b.tree_id_2) {
-        return true;
-      } else if (a.tree_id_2 == b.tree_id_2) {
-        if (a.ted_value < b.ted_value) {
-          return true;
-        }
-      }
-    }
-    return false;
-  });
-  // Convert vector to string.
-  std::string s;
-  for (auto e : v) {
-    s += "{" + std::to_string(e.tree_id_1) + "," + std::to_string(e.tree_id_2)
-        + "," + std::to_string(std::lround(e.ted_value)) + "},";
-  }
-  s.pop_back(); // Delete the last comma.
-  return s;
-}
-
 int main() {
   using Label = label::StringLabel;
   using CostModel = cost_model::UnitCostModel<Label>;
   using VerificationTouzet = ted::Touzet<Label, CostModel>;
 
-  // Correct result - currently hard-coded here.
-  std::string correct_result = "{0,1,1},{0,2,1},{1,2,1},{2,4,1}";
-
   // File path to input tree collection.
-  // std::string file_path = "guha_test_data.txt";
-  std::string file_path = "/home/mpawlik/Remote/ted-datasets/sentiment/sentiment_sorted.bracket";
+  std::string file_path = "join_test_data.txt";
+  // Correct results for bolzano dataset (threshold 1 to 15).
+  std::vector<unsigned int> results = {9, 37, 61, 109, 196, 344, 476, 596, 704, 840, 946, 1138, 1356, 1498, 1692};
 
-  // Set distance threshold - maximum number of allowed edit operations.
-  // double distance_threshold = 1.00;
+  // Create the container to store all trees.
+  std::vector<node::Node<Label>> trees_collection;
+  // Parse the dataset.
+  parser::BracketNotationParser bnp;
+  bnp.parse_collection(trees_collection, file_path);
   
-  for (int i = 1; i <= 1; i = i*2) {
+  // Execute for different thresholds.
+  // Currently only threshold 1 because it takes too long (75sec).
+  for (int i = 1; i < 2; i++) {
     std::vector<std::pair<unsigned int, unsigned int>> candidates;
     std::vector<join::JoinResultElement> join_result;
-
-    // Create the container to store all trees.
-    std::vector<node::Node<Label>> trees_collection;
-
-    // Parse the dataset.
-    parser::BracketNotationParser bnp;
-    bnp.parse_collection(trees_collection, file_path);
 
     // Guha with Touzet verification
     join::Guha<Label, CostModel, VerificationTouzet> guha;
     guha.execute_rsc_join(trees_collection, candidates, join_result, (double)i);
     
-    std::cout << "tau=" << i << "\tcand=" <<
-        candidates.size() << "\t|result|=" << join_result.size() <<
-        "\t#l_t_candidates=" << guha.get_l_t_candidates() <<
-        "\t#u_t_result_pairs=" << guha.get_u_t_result_pairs() <<
-        "\t#sed_candidates=" << guha.get_sed_candidates() <<
-        "\t#cted_result_pairs=" << guha.get_cted_result_pairs() << std::endl;
+    if(join_result.size() != results[i-1]) {
+      std::cout << " --- incorrect result for threshold " << i << ": " << join_result.size() 
+        << " instead of " << results[i-1] << std::endl;
+      return -1;
+    }
   }
-
-  
-  // std::vector<std::pair<unsigned int, unsigned int>> candidates;
-  // std::vector<join::JoinResultElement> join_result;
-  // 
-  // // Create the container to store all trees.
-  // std::vector<node::Node<Label>> trees_collection;
-  // 
-  // // Parse the dataset.
-  // parser::BracketNotationParser bnp;
-  // bnp.parse_collection(trees_collection, file_path);
-  // 
-  // // Guha with Touzet verification
-  // join::Guha<Label, CostModel, VerificationTouzet> guha;
-  // guha.execute_join(trees_collection, candidates, join_result, distance_threshold, 10);
-  
-  // std::string computed_result = vector_of_re_to_string(join_result);
-  // if (correct_result != computed_result) {
-  //   std::cerr << "Incorrect result set: " << computed_result << " instead of "
-  //       << correct_result << std::endl;
-  //   return -1;
-  // }
 
   return 0;
 }
